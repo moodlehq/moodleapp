1c0cbda1c275f0ccbed33bd7dc2e9bd2e7ef5269
Dani Palou <dpalou@cvaconsulting.com>
2015-03-24 11:47:52 +0100

    MOBILE-857 language: mmLang implemented and set angular-translate

--- www/core/lib/lang.js
+++ www/core/lib/lang.js
@@ -12,46 +12,59 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-angular.module('mm', ['ionic', 'mm.core', 'ngCordova'])
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-angular.module('mm.core', ['pascalprecht.translate']);
-
 angular.module('mm.core')
+
+/**
+ * @ngdoc service
+ * @name $mmLang
+ * @module mm.core
+ * @description
+ * This service allows to add new languages strings.
+ */
 .factory('$mmLang', function($translate, $translatePartialLoader, $mmConfig) {
+
     var self = {};
-        self.registerLanguageFolder = function(path) {
+
+    /**
+     * Register a folder to search language files into it.
+     *
+     * @param  {String} path Path of the folder to use.
+     */
+    self.registerLanguageFolder = function(path) {
         $translatePartialLoader.addPart(path);
     }
+
     self.changeCurrentLanguage = function(language) {
         $translate.use(language);
         $mmConfig.set('current_language', language);
     }
+
     return self;
 })
+
 .config(function($translateProvider, $translatePartialLoaderProvider) {
+
     $translateProvider.useLoader('$translatePartialLoader', {
       urlTemplate: '{part}/{lang}.json'
     });
+
+    // Load the built language files from build/lang.
     $translatePartialLoaderProvider.addPart('build/lang');
+
+    // Set fallback language.
     $translateProvider.fallbackLanguage('en');
 })
+
 .run(function($ionicPlatform, $translate, $cordovaGlobalization, $mmConfig) {
     $ionicPlatform.ready(function() {
+
+        // Get current language.
         $mmConfig.get('current_language').then(function(language) {
             $translate.use(language);
         }, function() {
             $cordovaGlobalization.getPreferredLanguage().then(function(result) {
                 var language = result.value;
+                // For now we won't support codes like en-US or es-US defined by locale.
                 if (language.indexOf('-') > -1) {
                     language = language.substr(0, language.indexOf('-'));
                 }
@@ -60,5 +73,6 @@ angular.module('mm.core')
                 $translate.use('en');
             });
         });
+
     });
 });
