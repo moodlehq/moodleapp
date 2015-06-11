af5c9a038a1a5e110b7a3c44d47f4482cc93de3a
Dani Palou <dpalou@cvaconsulting.com>
2015-06-11 15:41:54 +0200

    MOBILE-828 notifications: Notifications list implemented

---
+++ www/addons/notifications/filters/format.js
@@ -0,0 +1,30 @@
+// (C) Copyright 2015 Martin Dougiamas
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+angular.module('mm.addons.notifications')
+
+/**
+ * Filter to format a notification.
+ *
+ * @module mm.addons.notifications
+ * @ngdoc filter
+ * @name mmaNotificationsFormat
+ */
+.filter('mmaNotificationsFormat', function($mmText) {
+  return function(text) {
+    text = text.replace(/-{4,}/ig, '');
+    text = $mmText.replaceNewLines(text, '<br />');
+    return text;
+  };
+});
