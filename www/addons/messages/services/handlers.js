4b97b98588f2c3b665f127d971395ff3cff07e25
Dani Palou <dpalou@cvaconsulting.com>
2015-07-10 11:45:15 +0200

    MOBILE-1111 messages: Show errors in add/block contact

--- www/addons/messages/services/handlers.js
+++ www/addons/messages/services/handlers.js
@@ -23,7 +23,7 @@ angular.module('mm.addons.messages')
  * @ngdoc service
  * @name $mmaMessagesHandlers
  */
-.factory('$mmaMessagesHandlers', function($q, $log, $mmaMessages, $mmSite, $state) {
+.factory('$mmaMessagesHandlers', function($q, $log, $mmaMessages, $mmSite, $state, $mmUtil) {
     $log = $log.getInstance('$mmaMessagesHandlers');
 
     var self = {};
@@ -86,6 +86,8 @@ angular.module('mm.addons.messages')
                         } else {
                             return $mmaMessages.addContact(user.id);
                         }
+                    }).catch(function(error) {
+                        $mmUtil.showErrorModal(error);
                     }).finally(function() {
                         $rootScope.$broadcast('mmaMessagesHandlers:addUpdated');
                         updateTitle().finally(function() {
@@ -166,6 +168,8 @@ angular.module('mm.addons.messages')
                         } else {
                             return $mmaMessages.blockContact(user.id);
                         }
+                    }).catch(function(error) {
+                        $mmUtil.showErrorModal(error);
                     }).finally(function() {
                         $rootScope.$broadcast('mmaMessagesHandlers:blockUpdated');
                         updateTitle().finally(function() {
