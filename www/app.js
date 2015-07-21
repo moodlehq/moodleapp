a8cd71b0e788e404213b1cbc78761e46e24b5d05
Juan Leyva <juanleyvadelgado@gmail.com>
2015-07-21 12:02:55 +0200

    MOBILE-1124 accessibility: Aria plugin included

--- www/app.js
+++ www/app.js
@@ -14,7 +14,7 @@
 
 // Dependencies will be automatically added here, the following line must declare ionic as first dependency
 // and should not be split into multiple lines, see gulpfile.js.
-angular.module('mm', ['ionic', 'ngCordova', 'angular-md5', 'pascalprecht.translate'])
+angular.module('mm', ['ionic', 'ngCordova', 'angular-md5', 'pascalprecht.translate', 'ngAria'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
