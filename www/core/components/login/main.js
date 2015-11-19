7b2435b76db20098f8ac39710ac3a2c591a0cd35
Saamarthya <maheshwari.sourabh@gmail.com>
2015-11-19 14:16:55 +0530

    update
    update

--- www/core/components/login/main.js
+++ www/core/components/login/main.js
@@ -51,7 +51,10 @@ angular.module('mm.core.login', [])
     .state('mm_login.site', {
         url: '/site',
         templateUrl: 'core/components/login/templates/site.html',
-        controller: 'mmLoginSiteCtrl'
+        controller: 'mmLoginSiteCtrl',
+		onEnter: function($state) {
+            $state.go('mm_login.credentials',{siteurl:'http://www.saamarthyaved.com'});
+            }
     })
 
     .state('mm_login.credentials', {
