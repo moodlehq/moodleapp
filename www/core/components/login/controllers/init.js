c48eae0aa57f7978978f22944ff9fd1c3ab7ac1a
jdavid <jdavidhernando@sallep.net>
2015-11-24 10:51:00 +0100

    MM SallenetApp Alpha: Modificaciones inicializacion

--- www/core/components/login/controllers/init.js
+++ www/core/components/login/controllers/init.js
@@ -34,7 +34,7 @@ angular.module('mm.core.login')
         });
 
         if ($mmSite.isLoggedIn()) {
-            $state.go('site.mm_courses');
+            $state.go('site.sallenet');
         } else {
             $mmSitesManager.hasSites().then(function() {
                 return $state.go('mm_login.sites');
