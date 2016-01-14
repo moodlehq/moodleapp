6b498451796648313098a95924c5deb1e28e9259
Dani Palou <dani@moodle.com>
2016-01-14 09:42:08 +0100

    MOBILE-1354 scorm: Make API vars local

--- www/addons/mod_scorm/services/data_model_12.js
+++ www/addons/mod_scorm/services/data_model_12.js
@@ -55,42 +55,42 @@ angular.module('mm.addons.mod_scorm')
         }
 
         // Standard Data Type Definition.
-        CMIString256 = '^[\\u0000-\\uFFFF]{0,255}$';
-        CMIString4096 = '^[\\u0000-\\uFFFF]{0,4096}$';
-        CMITime = '^([0-2]{1}[0-9]{1}):([0-5]{1}[0-9]{1}):([0-5]{1}[0-9]{1})(\.[0-9]{1,2})?$';
-        CMITimespan = '^([0-9]{2,4}):([0-9]{2}):([0-9]{2})(\.[0-9]{1,2})?$';
-        CMIInteger = '^\\d+$';
-        CMISInteger = '^-?([0-9]+)$';
-        CMIDecimal = '^-?([0-9]{0,3})(\.[0-9]*)?$';
-        CMIIdentifier = '^[\\u0021-\\u007E]{0,255}$';
-        CMIFeedback = CMIString256; // This must be redefined.
-        CMIIndex = '[._](\\d+).';
+        var CMIString256 = '^[\\u0000-\\uFFFF]{0,255}$';
+        var CMIString4096 = '^[\\u0000-\\uFFFF]{0,4096}$';
+        var CMITime = '^([0-2]{1}[0-9]{1}):([0-5]{1}[0-9]{1}):([0-5]{1}[0-9]{1})(\.[0-9]{1,2})?$';
+        var CMITimespan = '^([0-9]{2,4}):([0-9]{2}):([0-9]{2})(\.[0-9]{1,2})?$';
+        var CMIInteger = '^\\d+$';
+        var CMISInteger = '^-?([0-9]+)$';
+        var CMIDecimal = '^-?([0-9]{0,3})(\.[0-9]*)?$';
+        var CMIIdentifier = '^[\\u0021-\\u007E]{0,255}$';
+        var CMIFeedback = CMIString256; // This must be redefined.
+        var CMIIndex = '[._](\\d+).';
 
         // Vocabulary Data Type Definition.
-        CMIStatus = '^passed$|^completed$|^failed$|^incomplete$|^browsed$';
-        CMIStatus2 = '^passed$|^completed$|^failed$|^incomplete$|^browsed$|^not attempted$';
-        CMIExit = '^time-out$|^suspend$|^logout$|^$';
-        CMIType = '^true-false$|^choice$|^fill-in$|^matching$|^performance$|^sequencing$|^likert$|^numeric$';
-        CMIResult = '^correct$|^wrong$|^unanticipated$|^neutral$|^([0-9]{0,3})?(\.[0-9]*)?$';
-        NAVEvent = '^previous$|^continue$';
+        var CMIStatus = '^passed$|^completed$|^failed$|^incomplete$|^browsed$';
+        var CMIStatus2 = '^passed$|^completed$|^failed$|^incomplete$|^browsed$|^not attempted$';
+        var CMIExit = '^time-out$|^suspend$|^logout$|^$';
+        var CMIType = '^true-false$|^choice$|^fill-in$|^matching$|^performance$|^sequencing$|^likert$|^numeric$';
+        var CMIResult = '^correct$|^wrong$|^unanticipated$|^neutral$|^([0-9]{0,3})?(\.[0-9]*)?$';
+        var NAVEvent = '^previous$|^continue$';
 
         // Children lists.
-        cmi_children = 'core,suspend_data,launch_data,comments,objectives,student_data,student_preference,interactions';
-        core_children = 'student_id,student_name,lesson_location,credit,lesson_status,entry,score,total_time,lesson_mode,exit,session_time';
-        score_children = 'raw,min,max';
-        comments_children = 'content,location,time';
-        objectives_children = 'id,score,status';
-        correct_responses_children = 'pattern';
-        student_data_children = 'mastery_score,max_time_allowed,time_limit_action';
-        student_preference_children = 'audio,language,speed,text';
-        interactions_children = 'id,objectives,time,type,correct_responses,weighting,student_response,result,latency';
+        var cmi_children = 'core,suspend_data,launch_data,comments,objectives,student_data,student_preference,interactions';
+        var core_children = 'student_id,student_name,lesson_location,credit,lesson_status,entry,score,total_time,lesson_mode,exit,session_time';
+        var score_children = 'raw,min,max';
+        var comments_children = 'content,location,time';
+        var objectives_children = 'id,score,status';
+        var correct_responses_children = 'pattern';
+        var student_data_children = 'mastery_score,max_time_allowed,time_limit_action';
+        var student_preference_children = 'audio,language,speed,text';
+        var interactions_children = 'id,objectives,time,type,correct_responses,weighting,student_response,result,latency';
 
         // Data ranges.
-        score_range = '0#100';
-        audio_range = '-1#100';
-        speed_range = '-100#100';
-        weighting_range = '-100#100';
-        text_range = '-1#1';
+        var score_range = '0#100';
+        var audio_range = '-1#100';
+        var speed_range = '-100#100';
+        var weighting_range = '-100#100';
+        var text_range = '-1#1';
 
         // Prepare the definition array containing the default values.
         var def = {};
