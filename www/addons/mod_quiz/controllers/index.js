1fea9cb0efa912267e5d06eae9ecafb8111405fb
Dani Palou <dani@moodle.com>
2016-02-09 13:58:43 +0100

    MOBILE-1406 quiz: Implement quiz entry page

---
+++ www/addons/mod_quiz/controllers/index.js
@@ -0,0 +1,258 @@
+// (C) Copyright 2015 Martin Dougiamas
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+angular.module('mm.addons.mod_quiz')
+
+/**
+ * Quiz index controller.
+ *
+ * @module mm.addons.mod_quiz
+ * @ngdoc controller
+ * @name mmaModQuizIndexCtrl
+ */
+.controller('mmaModQuizIndexCtrl', function($scope, $stateParams, $mmaModQuiz, $mmCourse, $ionicPlatform, $q, $translate,
+            $mmUtil, $mmaModQuizHelper) {
+    var module = $stateParams.module || {},
+        courseId = $stateParams.courseid,
+        quiz,
+        overallStats,
+        attempts,
+        options,
+        bestGrade,
+        gradebookData,
+        accessInfo,
+        moreAttempts;
+
+    $scope.title = module.name;
+    $scope.description = module.description;
+    $scope.moduleUrl = module.url;
+    $scope.isTablet = $ionicPlatform.isTablet();
+    $scope.courseId = courseId;
+
+    // Convenience function to get Quiz data.
+    function fetchQuizData(refresh) {
+        return $mmaModQuiz.getQuiz(courseId, module.id).then(function(quizData) {
+            quiz = quizData;
+            quiz.gradeMethodReadable = $mmaModQuiz.getQuizGradeMethod(quiz.grademethod);
+
+            $scope.now = new Date().getTime();
+            $scope.title = quiz.name || $scope.title;
+            $scope.description = quiz.intro || $scope.description;
+            $scope.quiz = quiz;
+
+            return getAttempts().catch(showError);
+
+        }, function(message) {
+            if (!refresh) {
+                // Get quiz failed, retry without using cache since it might be a new activity.
+                return refreshData();
+            }
+            return showError(message);
+        });
+    }
+
+    // Convenience function to get Quiz attempts.
+    function getAttempts() {
+
+        // Get access information of last attempt (it also works if no attempts made).
+        return $mmaModQuiz.getAccessInformation(quiz.id, 0).then(function(info) {
+            accessInfo = info;
+            $scope.accessRules = accessInfo.accessrules;
+            quiz.showReviewColumn = accessInfo.canreviewmyattempts;
+
+            // Get attempts.
+            return $mmaModQuiz.getUserAttempts(quiz.id).then(function(atts) {
+                attempts = atts;
+
+                return treatAttempts().then(function() {
+                    // Check if user can create/continue attempts.
+                    if (attempts.length) {
+                        var lastAttempt = attempts[attempts.length - 1];
+                        moreAttempts = !$mmaModQuiz.isAttemptFinished(lastAttempt.state) || !accessInfo.isfinished;
+                    } else {
+                        moreAttempts = !accessInfo.isfinished;
+                    }
+
+                    $scope.attempts = attempts;
+
+                    getButtonText();
+                    return getResultInfo();
+                });
+            });
+        });
+    }
+
+    // Treat user attempts.
+    function treatAttempts() {
+        if (!attempts || !attempts.length) {
+            return $q.when();
+        }
+
+        var lastFinished = $mmaModQuiz.getLastFinishedAttemptFromList(attempts),
+            promises = [];
+
+        // Get combined review options.
+        promises.push($mmaModQuiz.getCombinedReviewOptions(quiz.id).then(function(result) {
+            options = result;
+        }));
+
+        // Get best grade.
+        promises.push($mmaModQuiz.getUserBestGrade(quiz.id).then(function(best) {
+            bestGrade = best;
+        }));
+
+        // Get gradebook grade.
+        promises.push($mmaModQuiz.getGradeFromGradebook(courseId, module.id).then(function(data) {
+            gradebookData = data;
+        }));
+
+        return $q.all(promises).then(function() {
+            var quizGrade = typeof gradebookData.grade != 'undefined' ? gradebookData.grade : bestGrade.grade;
+
+            // Calculate data to construct the header of the attempts table.
+            $mmaModQuizHelper.setQuizCalculatedData(quiz, options);
+
+            overallStats = lastFinished && options.alloptions.marks >= $mmaModQuiz.QUESTION_OPTIONS_MARK_AND_MAX;
+
+            // Calculate data to show for each attempt.
+            angular.forEach(attempts, function(attempt) {
+                // Highlight the highest grade if appropriate.
+                var shouldHighlight = overallStats && quiz.grademethod == $mmaModQuiz.GRADEHIGHEST && attempts.length > 1;
+                $mmaModQuizHelper.setAttemptCalculatedData(quiz, attempt, shouldHighlight, quizGrade);
+            });
+        });
+    }
+
+    // Get result info to show.
+    function getResultInfo() {
+        if (attempts.length && quiz.showGradeColumn && bestGrade.hasgrade && typeof gradebookData.grade != 'undefined') {
+
+            $scope.showResults = true;
+            $scope.gradeOverridden = gradebookData.grade != bestGrade.grade;
+            $scope.gradebookFeedback = gradebookData.feedback;
+
+            if (overallStats) {
+                // Show the quiz grade. The message shown is different if the quiz is finished.
+                if (moreAttempts) {
+                    $scope.gradeResult = $translate.instant('mma.mod_quiz.gradesofar', {$a: {
+                        method: quiz.gradeMethodReadable,
+                        mygrade: $mmaModQuiz.formatGrade(gradebookData.grade, quiz.decimalpoints),
+                        quizgrade: quiz.gradeFormatted
+                    }});
+                } else {
+                    var outOfShort = $translate.instant('mma.mod_quiz.outofshort', {$a: {
+                        grade: $mmaModQuiz.formatGrade(gradebookData.grade, quiz.decimalpoints),
+                        maxgrade: quiz.gradeFormatted
+                    }});
+                    $scope.gradeResult = $translate.instant('mma.mod_quiz.yourfinalgradeis', {$a: outOfShort});
+                }
+            }
+
+            if (quiz.showFeedbackColumn) {
+                // Get the quiz overall feedback.
+                return $mmaModQuiz.getFeedbackForGrade(quiz.id, gradebookData.grade).then(function(response) {
+                    $scope.overallFeedback = response.feedbacktext;
+                });
+            }
+        } else {
+            $scope.showResults = false;
+        }
+        return $q.when();
+    }
+
+    // Get the text to show in the button. It also sets restriction messages if needed.
+    function getButtonText() {
+        $scope.buttonText = '';
+
+        if (quiz.hasquestions !== 0) {
+            if (attempts.length && !$mmaModQuiz.isAttemptFinished(attempts[attempts.length - 1].state)) {
+                // Last attempt is unfinished.
+                if (accessInfo.canattempt) {
+                    $scope.buttonText = 'mma.mod_quiz.continueattemptquiz';
+                } else if (accessInfo.canpreview) {
+                    $scope.buttonText = 'mma.mod_quiz.continuepreview';
+                }
+            } else {
+                // Last attempt is finished or no attempts.
+                if (accessInfo.canattempt) {
+                    $scope.preventMessages = accessInfo.preventnewattemptreasons;
+                    if (!$scope.preventMessages.length) {
+                        if (!attempts.length) {
+                            $scope.buttonText = 'mma.mod_quiz.attemptquiznow';
+                        } else {
+                            $scope.buttonText = 'mma.mod_quiz.reattemptquiz';
+                        }
+                    }
+                } else if (accessInfo.canpreview) {
+                    $scope.buttonText = 'mma.mod_quiz.previewquiznow';
+                }
+            }
+        }
+
+        if ($scope.buttonText) {
+            // So far we think a button should be printed, check if they will be allowed to access it.
+            $scope.preventMessages = accessInfo.preventaccessreasons;
+            if (!moreAttempts) {
+                $scope.buttonText = '';
+            } else if (accessInfo.canattempt && $scope.preventMessages.length) {
+                $scope.buttonText = '';
+            }
+        }
+    }
+
+    // Show error message and return a rejected promise.
+    function showError(message, defaultMessage) {
+        defaultMessage = defaultMessage || 'mma.mod_quiz.errorgetquiz';
+        if (message) {
+            $mmUtil.showErrorModal(message);
+        } else {
+            $mmUtil.showErrorModal(defaultMessage, true);
+        }
+        return $q.reject();
+    }
+
+    // Refreshes data.
+    function refreshData() {
+        var promises = [];
+        promises.push($mmaModQuiz.invalidateQuizData(courseId));
+        if (quiz) {
+            promises.push($mmaModQuiz.invalidateUserAttemptsForUser(quiz.id));
+            promises.push($mmaModQuiz.invalidateAccessInformationForAttempt(quiz.id, 0));
+            promises.push($mmaModQuiz.invalidateCombinedReviewOptionsForUser(quiz.id));
+            promises.push($mmaModQuiz.invalidateUserBestGradeForUser(quiz.id));
+            promises.push($mmaModQuiz.invalidateGradeFromGradebook(courseId));
+        }
+
+        return $q.all(promises).finally(function() {
+            return fetchQuizData(true);
+        });
+    }
+
+    // Fetch the Quiz data.
+    fetchQuizData().then(function() {
+        $mmaModQuiz.logViewQuiz(quiz.id).then(function() {
+            $mmCourse.checkModuleCompletion(courseId, module.completionstatus);
+        });
+    }).finally(function() {
+        $scope.quizLoaded = true;
+    });
+
+    // Pull to refresh.
+    $scope.refreshQuiz = function() {
+        refreshData().finally(function() {
+            $scope.$broadcast('scroll.refreshComplete');
+        });
+    };
+
+});
---
+++ www/addons/mod_quiz/services/helper.js
@@ -0,0 +1,100 @@
+// (C) Copyright 2015 Martin Dougiamas
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+angular.module('mm.addons.mod_scorm')
+
+/**
+ * Helper to gather some common quiz functions.
+ *
+ * @module mm.addons.mod_quiz
+ * @ngdoc service
+ * @name $mmaModQuizHelper
+ */
+.factory('$mmaModQuizHelper', function($mmaModQuiz, $mmUtil, $translate) {
+
+    var self = {};
+
+    /**
+     * Add some calculated data to the attempt.
+     *
+     * @module mm.addons.mod_quiz
+     * @ngdoc method
+     * @name $mmaModQuizHelper#setAttemptCalculatedData
+     * @param  {Object} quiz        Quiz.
+     * @param  {Object} attempt     Attempt.
+     * @param  {Boolean} highlight  True if we should check if attempt should be highlighted, false otherwise.
+     * @param  {Number} [bestGrade] Quiz's best grade. Required if highlight=true.
+     *                              the due date if the attempt's state is "overdue".
+     * @return {Void}
+     */
+    self.setAttemptCalculatedData = function(quiz, attempt, highlight, bestGrade) {
+
+        attempt.rescaledGrade = $mmaModQuiz.rescaleGrade(attempt.sumgrades, quiz, false);
+        attempt.finished = $mmaModQuiz.isAttemptFinished(attempt.state);
+        attempt.readableState = $mmaModQuiz.getAttemptReadableState(quiz, attempt);
+
+        if (quiz.showMarkColumn && attempt.finished) {
+            attempt.readableMark = $mmaModQuiz.formatGrade(attempt.sumgrades, quiz.decimalpoints);
+        } else {
+            attempt.readableMark = '';
+        }
+
+        if (quiz.showGradeColumn && attempt.finished) {
+            attempt.readableGrade = $mmaModQuiz.formatGrade(attempt.rescaledGrade, quiz.decimalpoints);
+            // Highlight the highest grade if appropriate.
+            attempt.highlightGrade = highlight && !attempt.preview && attempt.state == $mmaModQuiz.ATTEMPT_FINISHED &&
+                                        attempt.rescaledGrade == bestGrade;
+        } else {
+            attempt.readableGrade = '';
+        }
+    };
+
+    /**
+     * Add some calculated data to the quiz.
+     *
+     * @module mm.addons.mod_quiz
+     * @ngdoc method
+     * @name $mmaModQuizHelper#setQuizCalculatedData
+     * @param  {Object} quiz    Quiz.
+     * @param  {Object} options Options returned by $mmaModQuiz#getCombinedReviewOptions.
+     * @return {Void}
+     */
+    self.setQuizCalculatedData = function(quiz, options) {
+        quiz.sumGradesFormatted = $mmaModQuiz.formatGrade(quiz.sumgrades, quiz.decimalpoints);
+        quiz.gradeFormatted = $mmaModQuiz.formatGrade(quiz.grade, quiz.decimalpoints);
+
+        quiz.showAttemptColumn = quiz.attempts != 1;
+        quiz.showGradeColumn = options.someoptions.marks >= $mmaModQuiz.QUESTION_OPTIONS_MARK_AND_MAX &&
+                                    $mmaModQuiz.quizHasGrades(quiz);
+        quiz.showMarkColumn = quiz.showGradeColumn && quiz.grade != quiz.sumgrades;
+        quiz.showFeedbackColumn = quiz.hasfeedback && options.alloptions.overallfeedback;
+    };
+
+    /**
+     * Show error because a SCORM download failed.
+     *
+     * @module mm.addons.mod_scorm
+     * @ngdoc method
+     * @name $mmaModScormHelper#showDownloadError
+     * @param {Object} scorm SCORM downloaded.
+     * @return {Void}
+     */
+    self.showDownloadError = function(scorm) {
+        $translate('mma.mod_scorm.errordownloadscorm', {name: scorm.name}).then(function(message) {
+            $mmUtil.showErrorModal(message);
+        });
+    };
+
+    return self;
+});
