c5d2bd6542b674765b85557f550e1dd04f125567
Dani Palou <dani@moodle.com>
2016-02-15 14:26:16 +0100

    MOBILE-1420 quiz: Implement description and essay

---
+++ www/addons/mod_quiz/questions/description/directive.js
@@ -0,0 +1,43 @@
+// (C) Copyright 2015 Martin Dougiamas
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+angular.module('mm.addons.mod_quiz')
+
+/**
+ * Directive to render a description question.
+ *
+ * @module mm.addons.mod_quiz
+ * @ngdoc directive
+ * @name mmaModQuizQuestionDescription
+ */
+.directive('mmaModQuizQuestionDescription', function($log, $mmaModQuestionHelper) {
+	$log = $log.getInstance('mmaModQuizQuestionDescription');
+
+    return {
+        restrict: 'A',
+        priority: 100,
+        templateUrl: 'addons/mod_quiz/questions/description/template.html',
+        link: function(scope) {
+            var questionEl = $mmaModQuestionHelper.directiveInit(scope, $log),
+                input;
+            if (questionEl) {
+                // Get the "seen" hidden input.
+                input = questionEl[0].querySelector('input[type="hidden"][name*=seen]');
+                if (input) {
+                    scope.answers[input.name] = input.value;
+                }
+            }
+        }
+    };
+});
