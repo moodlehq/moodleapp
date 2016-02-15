dcf7e263e40f64c35ef4f933fef094062395b1b0
Dani Palou <dani@moodle.com>
2016-02-15 13:47:36 +0100

    MOBILE-1420 quiz: Implement multichoice, truefalse and calculatedmulti

---
+++ www/addons/mod_quiz/questions/calculatedmulti/handlers.js
@@ -0,0 +1,58 @@
+// (C) Copyright 2015 Martin Dougiamas
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+angular.module('mm.addons.mod_quiz')
+
+/**
+ * Calculated multi question handlers.
+ *
+ * @module mm.addons.mod_quiz
+ * @ngdoc service
+ * @name $mmaModQuizQuestionCalculatedMultiHandler
+ */
+.factory('$mmaModQuizQuestionCalculatedMultiHandler', function() {
+
+    var self = {};
+
+    /**
+     * Whether or not the module is enabled for the site.
+     *
+     * @return {Boolean}
+     */
+    self.isEnabled = function() {
+        return true;
+    };
+
+    /**
+     * Get the directive.
+     *
+     * @param {Object} question The question.
+     * @return {String}         Directive's name.
+     */
+    self.getDirectiveName = function(question) {
+        return 'mma-mod-quiz-question-calculated-multi';
+    };
+
+    return self;
+})
+
+.run(function($mmAddonManager) {
+    // It shouldn't be mandatory for us to inject the delegate using $mmAddonManager because we're inside mod_quiz addon,
+    // but this way it can be used as an example for external question handlers.
+    var $mmaModQuizQuestionsDelegate = $mmAddonManager.get('$mmaModQuizQuestionsDelegate');
+    if ($mmaModQuizQuestionsDelegate) {
+        $mmaModQuizQuestionsDelegate.registerHandler('mmaModQuizCalculatedMulti', 'qtype_calculatedmulti',
+                                                        '$mmaModQuizQuestionCalculatedMultiHandler');
+    }
+});
