18f2f0bf7cf908c72755c74c3747ebeab6f96b45
Albert Gasset <agasset@cvaconsulting.com>
2016-05-12 12:14:01 +0200

    MOBILE-1545: Support to log in via SSO with an embedded browser

--- www/core/components/login/controllers/credentials.js
+++ www/core/components/login/controllers/credentials.js
@@ -52,7 +52,7 @@ angular.module('mm.core.login')
                 // Check that there's no SSO authentication ongoing and the view hasn't changed.
                 if (!$mmLoginHelper.isSSOLoginOngoing() && !$scope.$$destroyed) {
                     $mmUtil.showConfirm($translate('mm.login.logininsiterequired')).then(function() {
-                        $mmLoginHelper.openBrowserForSSOLogin(result.siteurl);
+                        $mmLoginHelper.openBrowserForSSOLogin(result.siteurl, result.code);
                     });
                 }
             } else {
