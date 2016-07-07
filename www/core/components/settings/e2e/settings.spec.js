dac2968c0a7bed48feb473876ec4ff18724bc531
Juan Leyva <juanleyvadelgado@gmail.com>
2016-07-07 11:06:37 +0100

    MOBILE-1682 e2e: Settings tests fixes

--- www/core/components/settings/e2e/settings.spec.js
+++ www/core/components/settings/e2e/settings.spec.js
@@ -1,13 +1,22 @@
-/**
- * Created by Supun
- */
+// (C) Copyright 2015 Martin Dougiamas
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 describe('User can change App settings', function() {
 
     it('User can click settings page tabs', function (done) {
         return MM.loginAsStudent().then(function () {
             return MM.clickOnInSideMenu('App settings');
-            return element(by.xpath('/html/body/ion-nav-view/ion-side-menus/ion-side-menu-content/ion-nav-bar/div[2]/ion-header-bar/button')).click();
         }).then(function () {
             return MM.clickOn('General');
         }).then(function() {
@@ -32,11 +41,10 @@ describe('User can change App settings', function() {
     it('Change general settings', function (done) {
         return MM.loginAsStudent().then(function () {
             return MM.clickOnInSideMenu('App settings');
-            return element(by.xpath('/html/body/ion-nav-view/ion-side-menus/ion-side-menu-content/ion-nav-bar/div[2]/ion-header-bar/button')).click();
         }).then(function () {
             return MM.clickOn('General');
         }).then(function () {
-            return $('[ng-model="selectedLanguage"]').click();
+            return element(by.model('selectedLanguage')).click();
         }).then(function () {
             return element(by.xpath('/html/body/ion-nav-view/ion-side-menus/ion-side-menu-content/ion-nav-view/ion-view[3]/ion-content/div[1]/ul/li[1]/select/option[9]')).click();
         }).then(function () {
@@ -44,7 +52,7 @@ describe('User can change App settings', function() {
         }).then(function () {
             return MM.clickOn('General');
         }).then(function () {
-            return $('[ng-model="selectedLanguage"]').click();
+            return element(by.model('selectedLanguage')).click();
         }).then(function () {
             return element(by.xpath('/html/body/ion-nav-view/ion-side-menus/ion-side-menu-content/ion-nav-view/ion-view[2]/ion-content/div[1]/ul/li[1]/select/option[7]')).click();
         }).then(function () {
@@ -61,7 +69,6 @@ describe('User can change App settings', function() {
     it('Change Space usage settings', function (done) {
         return MM.loginAsStudent().then(function () {
             return MM.clickOnInSideMenu('App settings');
-            return element(by.xpath('/html/body/ion-nav-view/ion-side-menus/ion-side-menu-content/ion-nav-bar/div[2]/ion-header-bar/button')).click();
         }).then(function () {
             return MM.clickOn('Space usage');
         }).then(function() {
@@ -74,7 +81,6 @@ describe('User can change App settings', function() {
     it('Change Synchronization settings', function (done) {
         return MM.loginAsStudent().then(function () {
             return MM.clickOnInSideMenu('App settings');
-            return element(by.xpath('/html/body/ion-nav-view/ion-side-menus/ion-side-menu-content/ion-nav-bar/div[2]/ion-header-bar/button')).click();
         }).then(function () {
             return MM.clickOn('Synchronization');
             expect(MM.getView().getText()).toMatch('Synchronization settings');
@@ -90,7 +96,6 @@ describe('User can change App settings', function() {
     it('Change About settings', function (done) {
         return MM.loginAsStudent().then(function () {
             return MM.clickOnInSideMenu('App settings');
-            return element(by.xpath('/html/body/ion-nav-view/ion-side-menus/ion-side-menu-content/ion-nav-bar/div[2]/ion-header-bar/button')).click();
         }).then(function () {
             return MM.clickOn('About');
         }).then(function () {
