24f78cca7cef5fc941adf3a11248d8d789e87f68
Juan Leyva <juanleyvadelgado@gmail.com>
2016-07-07 11:06:11 +0100

    MOBILE-1682 e2e: Quiz tests fixes

--- www/addons/mod/quiz/e2e/quiz.spec.js
+++ www/addons/mod/quiz/e2e/quiz.spec.js
@@ -1,10 +1,20 @@
-/**
- * Created by Supun
- */
+// (C) Copyright 2015 Martin Dougiamas
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-describe('User can attempt questions', function() {
+describe('User can attempt quizzes', function() {
 
-    it('View question windows', function (done) {
+    it('Open a quiz', function (done) {
         return MM.loginAsStudent().then(function () {
             return MM.clickOnInSideMenu('My courses');
         }).then(function () {
@@ -19,7 +29,7 @@ describe('User can attempt questions', function() {
         });
     });
 
-    it('Attept Questions', function (done) {
+    it('Attept Quiz', function (done) {
         return MM.loginAsStudent().then(function () {
             return MM.clickOnInSideMenu('My courses');
         }).then(function () {
@@ -90,21 +100,6 @@ describe('User can attempt questions', function() {
             return element(by.xpath('//*[@id="mma-mod_quiz-question-1"]/mm-question/div[1]/section/label[3]/label/div/i')).click();
         }).then(function () {
             return MM.clickOn('Check');
-            done();
-        });
-    });
-
-    it('User can open test in browser', function (done) {
-        return MM.loginAsStudent().then(function () {
-            return MM.clickOnInSideMenu('My courses');
-        }).then(function () {
-            return MM.clickOn('Psychology in Cinema');
-        }).then(function () {
-            return MM.clickOn('Group Projects and Individual tasks');
-        }).then(function () {
-            return MM.clickOn('Grammar help with your essays');
-        }).then(function() {
-            return element(by.xpath('/html/body/ion-nav-view/ion-side-menus/ion-side-menu-content/ion-nav-bar/div[2]/ion-header-bar/div[3]/span/a')).click();
             done();
         });
     });
