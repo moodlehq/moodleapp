deb434ce951175f85b4fe7b9dc8d981c804ee9e4
Pau Ferrer Ocaña <crazyserver@gmail.com>
2016-09-05 09:35:41 +0200

    MOBILE-1743 choice: Implement choice sync

--- www/addons/mod/choice/main.js
+++ www/addons/mod/choice/main.js
@@ -19,6 +19,7 @@ angular.module('mm.addons.mod_choice', [])
 .constant('mmaModChoiceResultsAfterClose', 2)
 .constant('mmaModChoiceResultsAlways', 3)
 .constant('mmaModChoiceComponent', 'mmaModChoice')
+.constant('mmaModChoiceAutomSyncedEvent', 'mma-mod_choice_autom_synced')
 
 .config(function($stateProvider) {
 
@@ -44,4 +45,7 @@ angular.module('mm.addons.mod_choice', [])
     $mmCourseDelegateProvider.registerContentHandler('mmaModChoice', 'choice', '$mmaModChoiceHandlers.courseContent');
     $mmContentLinksDelegateProvider.registerLinkHandler('mmaModChoice', '$mmaModChoiceHandlers.linksHandler');
     $mmCoursePrefetchDelegateProvider.registerPrefetchHandler('mmaModChoice', 'choice', '$mmaModChoicePrefetchHandler');
+})
+.run(function($mmCronDelegate) {
+    $mmCronDelegate.register('mmaModChoice', '$mmaModChoiceHandlers.syncHandler');
 });
