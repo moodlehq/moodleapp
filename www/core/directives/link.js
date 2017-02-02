d0edfeea97135e9632e326a77314adc3d818fc12
Dani Palou <dani@moodle.com>
2017-02-02 13:17:13 +0100

    MOBILE-1959 sidemenu: Allow browser custom items

--- www/core/directives/link.js
+++ www/core/directives/link.js
@@ -22,6 +22,7 @@ angular.module('mm.core')
  * @name mmLink
  *
  * @param {Boolean} [captureLink=false] If the link needs to be captured by the app.
+ * @param {Boolean} [inApp=false]       True to open in embedded browser, false to open in system browser.
  * @param {String} [autoLogin=check] If the link should be open with auto-login. Accepts the following values:
  *                                   "yes" -> Always auto-login.
  *                                   "no" -> Never auto-login.
@@ -33,9 +34,11 @@ angular.module('mm.core')
      * Convenience function to correctly navigate, open file or url in the browser.
      *
      * @param  {String} href              HREF to be opened.
+     * @param  {Mixed} [inApp]            True to open in embedded browser, false to open in system browser.
      * @param  {String} [autoLogin=check] Whether to auto-login. "yes", "no" or "check".
      */
-    function navigate(href, autoLogin) {
+    function navigate(href, inApp, autoLogin) {
+        inApp = inApp && inApp !== 'false';
         autoLogin = autoLogin || 'check';
 
         if (href.indexOf('cdvfile://') === 0 || href.indexOf('file://') === 0) {
@@ -56,13 +59,29 @@ angular.module('mm.core')
             // It's an external link, we will open with browser. Check if we need to auto-login.
             if (!$mmSite.isLoggedIn()) {
                 // Not logged in, cannot auto-login.
-                $mmUtil.openInBrowser(href);
+                if (inApp) {
+                    $mmUtil.openInApp(href);
+                } else {
+                    $mmUtil.openInBrowser(href);
+                }
             } else if (autoLogin == 'yes') {
-                $mmSite.openInBrowserWithAutoLogin(href);
+                if (inApp) {
+                    $mmSite.openInAppWithAutoLogin(href);
+                } else {
+                    $mmSite.openInBrowserWithAutoLogin(href);
+                }
             } else if (autoLogin == 'no') {
-                $mmUtil.openInBrowser(href);
+                if (inApp) {
+                    $mmUtil.openInApp(href);
+                } else {
+                    $mmUtil.openInBrowser(href);
+                }
             } else {
-                $mmSite.openInBrowserWithAutoLoginIfSameSite(href);
+                if (inApp) {
+                    $mmSite.openInAppWithAutoLoginIfSameSite(href);
+                } else {
+                    $mmSite.openInBrowserWithAutoLoginIfSameSite(href);
+                }
             }
         }
     }
@@ -82,11 +101,11 @@ angular.module('mm.core')
                         if (attrs.captureLink && attrs.captureLink !== 'false') {
                             $mmContentLinksHelper.handleLink(href).then(function(treated) {
                                 if (!treated) {
-                                   navigate(href, attrs.autoLogin);
+                                   navigate(href, attrs.inApp, attrs.autoLogin);
                                 }
                             });
                         } else {
-                            navigate(href, attrs.autoLogin);
+                            navigate(href, attrs.inApp, attrs.autoLogin);
                         }
                     }
                 }
