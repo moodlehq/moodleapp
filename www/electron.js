fff34d26c9d678b3c288e94dc954695d00ccbcf5
Dani Palou <dani@moodle.com>
2017-06-27 16:10:52 +0200

    MOBILE-2148 electron: Read customurlscheme from config.json

--- www/electron.js
+++ www/electron.js
@@ -2,6 +2,7 @@
 const {app, BrowserWindow, ipcMain, shell} = require('electron');
 const path = require('path');
 const url = require('url');
+const fs = require('fs');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -67,8 +68,18 @@ app.on('activate', () => {
     }
 });
 
-// Open the app when a link with "moodlemobile://" is clicked.
-app.setAsDefaultProtocolClient('moodlemobile');
+// Read the config.json file to set app's protocol (custom URL scheme).
+fs.readFile(path.join(__dirname, 'config.json'), 'utf8', (err, data) => {
+    var ssoScheme = 'moodlemobile'; // Default value.
+    if (!err) {
+        try {
+            data = JSON.parse(data);
+            ssoScheme = data.customurlscheme;
+        } catch(ex) {}
+    }
+
+    app.setAsDefaultProtocolClient(ssoScheme);
+});
 
 // Make sure that only a single instance of the app is running.
 var shouldQuit = app.makeSingleInstance((argv, workingDirectory) => {
