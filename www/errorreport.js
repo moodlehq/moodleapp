42beccda6f57b67168c94b6035ad1de3ae1eea87
Wail Abualela <wailabualela@gmail.com>
2018-01-16 13:59:29 +0300

    wail

--- www/errorreport.js
+++ www/errorreport.js
@@ -16,7 +16,7 @@
 // Using JS confirm function we are sure that the user get notified in a Mobile device.
 // This script should be added at the begining of the index.html and it should only use native javascript functions.
 
-var appVersion = '3.4.0 (2021)',
+var appVersion = '1.0.0 (101)',
     reportInBackgroundName = 'mmCoreReportInBackground',
     errors = [],
     ignoredFiles = ['www/index.html#/site/mod_page', 'www/index.html#/site/mod_resource', 'www/index.html#/site/mm_course-section'],
@@ -55,7 +55,7 @@ function getStorageAndReport(reportUrl, db) {
 function sendErrorReport(msg, url, lineNumber) {
     url = url || "";
     lineNumber = lineNumber || 0;
-    var reportUrl = 'http://prototype.moodle.net/mobile/feedback/mmfeedback.php?message=' + encodeURIComponent(msg) +
+    var reportUrl = 'http://ust.elearning.net/feedback.php?message=' + encodeURIComponent(msg) +
                     '&file=' + encodeURIComponent(url) + '&line=' + encodeURIComponent(lineNumber) + '&appv=' +
                     encodeURIComponent(appVersion) + '&bg=1';
 
