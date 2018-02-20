b54860f744a365f35935f516948d89fbfdf34c6a
Héctor de Jesus Basora Arriaga <h.basora@hotmail.com>
2018-02-20 14:27:50 -0400

    Update errorreport.js

--- www/errorreport.js
+++ www/errorreport.js
@@ -72,7 +72,7 @@ window.onerror = function(msg, url, lineNumber) {
         function reportError() {
             if (!errorReported) {
                 errorReported = true;
-                var reportUrl = 'http://prototype.moodle.net/mobile/feedback/mmfeedback.php?message=' + encodeURIComponent(msg) +
+                var reportUrl = 'http://www.focadepro.com/mobile/feedback/mmfeedback.php?message=' + encodeURIComponent(msg) +
                                 '&file=' + encodeURIComponent(url) + '&line=' + encodeURIComponent(lineNumber) + '&appv=' +
                                 encodeURIComponent(appVersion) + '&bg=' + (reportInBackground ? 1 : 0);
 
