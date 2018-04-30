92182ed720e2d0a5a2699580e7f64b42be2973de
Dani Palou <dani@moodle.com>
2018-04-30 12:16:28 +0200

    MOBILE-2345 lesson: Implement sync provider and prefetch handler

---
+++ src/addon/mod/lesson/pages/password-modal/password-modal.ts
@@ -0,0 +1,43 @@
+// (C) Copyright 2015 Martin Dougiamas
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { Component } from '@angular/core';
+import { IonicPage, ViewController } from 'ionic-angular';
+
+/**
+ * Modal that asks the password for a lesson.
+ */
+@IonicPage({ segment: 'addon-mod-lesson-password-modal' })
+@Component({
+    selector: 'page-addon-mod-lesson-password-modal',
+    templateUrl: 'password-modal.html',
+})
+export class AddonModLessonPasswordModalPage {
+
+    constructor(protected viewCtrl: ViewController) { }
+
+    /**
+     * Send the password back.
+     */
+    submitPassword(password: HTMLInputElement): void {
+        this.viewCtrl.dismiss(password.value);
+    }
+
+    /**
+     * Close modal.
+     */
+    closeModal(): void {
+        this.viewCtrl.dismiss();
+    }
+}
