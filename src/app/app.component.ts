5514e56f00abcee277540127de1bb95cf275f4e1
Pau Ferrer Ocaña <crazyserver@gmail.com>
2018-11-19 14:22:58 +0100

    MOBILE-2739 site: Add site version to body tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -62,6 +62,9 @@ export class MoodleMobileApp implements OnInit {
 
             // Unload lang custom strings.
             this.langProvider.clearCustomStrings();
+
+            // Remove version classes from body.
+            this.removeVersionClass();
         });
 
         // Listen for session expired events.
@@ -142,6 +145,10 @@ export class MoodleMobileApp implements OnInit {
         this.eventsProvider.on(CoreEventsProvider.SITE_UPDATED, (data) => {
             if (data.siteId == this.sitesProvider.getCurrentSiteId()) {
                 loadCustomStrings();
+
+                // Add version classes to body.
+                this.removeVersionClass();
+                this.addVersionClass(this.sitesProvider.getReleaseNumber(data.release || ''));
             }
         });
 
@@ -167,6 +174,38 @@ export class MoodleMobileApp implements OnInit {
             };
 
             pauseVideos(window);
+        });
+    }
+
+    /**
+     * Convenience function to add version to body classes.
+     *
+     * @param {string} release Current release number of the site.
+     */
+    protected addVersionClass(release: string): void {
+        const parts = release.split('.');
+
+        parts[1] = parts[1] || '0';
+        parts[2] = parts[2] || '0';
+
+        document.body.classList.add('version-' + parts[0], 'version-' + parts[0] + '-' + parts[1],
+            'version-' + parts[0] + '-' + parts[1] + '-' + parts[2]);
+
+    }
+
+    /**
+     * Convenience function to remove all version classes form body.
+     */
+    protected removeVersionClass(): void {
+        const remove = [];
+        Array.from(document.body.classList).forEach((tempClass) => {
+            if (tempClass.substring(0, 8) == 'version-') {
+                remove.push(tempClass);
+            }
+        });
+
+        remove.forEach((tempClass) => {
+            document.body.classList.remove(tempClass);
         });
     }
 }
