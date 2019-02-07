e987aeba9896d803af1d4c3d3c9c9d00c0d39751
Albert Gasset <albertgasset@fsfe.org>
2019-02-07 17:18:19 +0100

    MOBILE-1928 chat: Passed sessions

--- src/addon/mod/chat/providers/chat.ts
+++ src/addon/mod/chat/providers/chat.ts
@@ -13,9 +13,12 @@
 // limitations under the License.
 
 import { Injectable } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 import { CoreSitesProvider } from '@providers/sites';
 import { CoreUserProvider } from '@core/user/providers/user';
 import { CoreCourseLogHelperProvider } from '@core/course/providers/log-helper';
+import { CoreUtilsProvider } from '@providers/utils/utils';
+import { CoreSiteWSPreSets } from '@classes/site';
 
 /**
  * Service that provides some features for chats.
@@ -25,34 +28,38 @@ export class AddonModChatProvider {
     static COMPONENT = 'mmaModChat';
     static POLL_INTERVAL = 4000;
 
+    protected ROOT_CACHE_KEY = 'AddonModChat:';
+
     constructor(private sitesProvider: CoreSitesProvider, private userProvider: CoreUserProvider,
-        private logHelper: CoreCourseLogHelperProvider) {}
+        private logHelper: CoreCourseLogHelperProvider, protected utils: CoreUtilsProvider, private translate: TranslateService) {}
 
     /**
      * Get a chat.
      *
-     * @param  {number}  courseId        Course ID.
-     * @param  {number}  cmId            Course module ID.
-     * @param  {boolean} [refresh=false] True when we should not get the value from the cache.
+     * @param {number} courseId Course ID.
+     * @param {number} cmId Course module ID.
+     * @param {string} [siteId] Site ID. If not defined, current site.
      * @return {Promise<any>} Promise resolved when the chat is retrieved.
      */
-    getChat(courseId: number, cmId: number, refresh: boolean = false): Promise<any> {
-        const params = {
-            courseids: [courseId]
-        };
-        const preSets = {
-            getFromCache: refresh ? false : undefined,
-        };
+    getChat(courseId: number, cmId: number, siteId?: string): Promise<any> {
+        return this.sitesProvider.getSite(siteId).then((site) => {
+            const params = {
+                courseids: [courseId]
+            };
+            const preSets: CoreSiteWSPreSets = {
+                cacheKey: this.getChatsCacheKey(courseId)
+            };
 
-        return this.sitesProvider.getCurrentSite().read('mod_chat_get_chats_by_courses', params, preSets).then((response) => {
-            if (response.chats) {
-                const chat = response.chats.find((chat) => chat.coursemodule == cmId);
-                if (chat) {
-                    return chat;
+            return site.read('mod_chat_get_chats_by_courses', params, preSets).then((response) => {
+                if (response.chats) {
+                    const chat = response.chats.find((chat) => chat.coursemodule == cmId);
+                    if (chat) {
+                        return chat;
+                    }
                 }
-            }
 
-            return Promise.reject(null);
+                return Promise.reject(null);
+            });
         });
     }
 
@@ -146,8 +153,8 @@ export class AddonModChatProvider {
                 message.userfullname = user.fullname;
                 message.userprofileimageurl = user.profileimageurl;
             }).catch(() => {
-                // Error getting profile. Set default data.
-                message.userfullname = message.userid;
+                // Error getting profile, most probably the user is deleted.
+                message.userfullname = this.translate.instant('core.deleteduser', {$a: message.userid});
             });
         });
 
@@ -171,5 +178,211 @@ export class AddonModChatProvider {
         };
 
         return this.sitesProvider.getCurrentSite().read('mod_chat_get_chat_users', params, preSets);
+    }
+
+    /**
+     * Return whether WS for passed sessions are available.
+     *
+     * @param {string} [siteId] Site ID. If not defined, current site.
+     * @return {Promise<boolean>} Promise resolved with a boolean.
+     */
+    areSessionsAvailable(siteId?: string): Promise<boolean> {
+        return this.sitesProvider.getSite(siteId).then((site) => {
+            return site.wsAvailable('mod_chat_get_sessions') && site.wsAvailable('mod_chat_get_session_messages');
+        });
+    }
+
+    /**
+     * Get chat sessions.
+     *
+     * @param {number} chatId Chat ID.
+     * @param {number} [groupId=0] Group ID, 0 means that the function will determine the user group.
+     * @param {boolean} [showAll=false] Whether to include incomplete sessions or not.
+     * @param {boolean} [ignoreCache=false] True if it should ignore cached data (it will always fail in offline or server down).
+     * @param {string} [siteId] Site ID. If not defined, current site.
+     * @return {Promise<any[]>} Promise resolved with the list of sessions.
+     * @since 3.5
+     */
+    getSessions(chatId: number, groupId: number = 0, showAll: boolean = false, ignoreCache: boolean = false, siteId?: string):
+            Promise<any[]> {
+        return this.sitesProvider.getSite(siteId).then((site) => {
+            const params = {
+                chatid: chatId,
+                groupid: groupId,
+                showall: showAll ? 1 : 0
+            };
+            const preSets: CoreSiteWSPreSets = {
+                cacheKey: this.getSessionsCacheKey(chatId, groupId, showAll),
+            };
+            if (ignoreCache) {
+                preSets.getFromCache = false;
+                preSets.emergencyCache = false;
+            }
+
+            return site.read('mod_chat_get_sessions', params, preSets).then((response) => {
+                if (!response || !response.sessions) {
+                    return Promise.reject(null);
+                }
+
+                return response.sessions;
+            });
+        });
+    }
+
+    /**
+     * Get chat session messages.
+     *
+     * @param {number} chatId Chat ID.
+     * @param {number} sessionStart Session start time.
+     * @param {number} sessionEnd Session end time.
+     * @param {number} [groupId=0] Group ID, 0 means that the function will determine the user group.
+     * @param {boolean} [ignoreCache=false] True if it should ignore cached data (it will always fail in offline or server down).
+     * @param {string} [siteId] Site ID. If not defined, current site.
+     * @return {Promise<any[]>} Promise resolved with the list of messages.
+     * @since 3.5
+     */
+    getSessionMessages(chatId: number, sessionStart: number, sessionEnd: number, groupId: number = 0, ignoreCache: boolean = false,
+            siteId?: string): Promise<any[]> {
+        return this.sitesProvider.getSite(siteId).then((site) => {
+            const params = {
+                chatid: chatId,
+                sessionstart: sessionStart,
+                sessionend: sessionEnd,
+                groupid: groupId
+            };
+            const preSets: CoreSiteWSPreSets = {
+                cacheKey: this.getSessionMessagesCacheKey(chatId, sessionStart, groupId)
+            };
+            if (ignoreCache) {
+                preSets.getFromCache = false;
+                preSets.emergencyCache = false;
+            }
+
+            return site.read('mod_chat_get_session_messages', params, preSets).then((response) => {
+                if (!response || !response.messages) {
+                    return Promise.reject(null);
+                }
+
+                return response.messages;
+            });
+        });
+    }
+
+    /**
+     * Invalidate chats.
+     *
+     * @param {number} courseId Course ID.
+     * @return {Promise<any>} Promise resolved when the data is invalidated.
+     */
+    invalidateChats(courseId: number): Promise<any> {
+        const site = this.sitesProvider.getCurrentSite();
+
+        return site.invalidateWsCacheForKey(this.getChatsCacheKey(courseId));
+    }
+
+    /**
+     * Invalidate chat sessions.
+     *
+     * @param {number} chatId Chat ID.
+     * @param {number} [groupId=0] Group ID, 0 means that the function will determine the user group.
+     * @param {boolean} [showAll=false] Whether to include incomplete sessions or not.
+     * @return {Promise<any>} Promise resolved when the data is invalidated.
+     */
+    invalidateSessions(chatId: number, groupId: number = 0, showAll: boolean = false): Promise<any> {
+        const site = this.sitesProvider.getCurrentSite();
+
+        return site.invalidateWsCacheForKey(this.getSessionsCacheKey(chatId, groupId, showAll));
+    }
+
+    /**
+     * Invalidate all chat sessions.
+     *
+     * @param {number} chatId Chat ID.
+     * @return {Promise<any>} Promise resolved when the data is invalidated.
+     */
+    invalidateAllSessions(chatId: number): Promise<any> {
+        const site = this.sitesProvider.getCurrentSite();
+
+        return site.invalidateWsCacheForKeyStartingWith(this.getSessionsCacheKeyPrefix(chatId));
+    }
+
+    /**
+     * Invalidate chat session messages.
+     *
+     * @param {number} chatId Chat ID.
+     * @param {number} sessionStart Session start time.
+     * @param {number} [groupId=0] Group ID, 0 means that the function will determine the user group.
+     * @return {Promise<any>} Promise resolved when the data is invalidated.
+     */
+    invalidateSessionMessages(chatId: number, sessionStart: number, groupId: number = 0): Promise<any> {
+        const site = this.sitesProvider.getCurrentSite();
+
+        return site.invalidateWsCacheForKey(this.getSessionMessagesCacheKey(chatId, sessionStart, groupId));
+    }
+
+    /**
+     * Invalidate all chat session messages.
+     *
+     * @param {number} chatId Chat ID.
+     * @return {Promise<any>} Promise resolved when the data is invalidated.
+     */
+    invalidateAllSessionMessages(chatId: number): Promise<any> {
+        const site = this.sitesProvider.getCurrentSite();
+
+        return site.invalidateWsCacheForKeyStartingWith(this.getSessionMessagesCacheKeyPrefix(chatId));
+    }
+
+    /**
+     * Get cache key for chats WS call.
+     *
+     * @param {number} courseId Course ID.
+     * @return {string} Cache key.
+     */
+    protected getChatsCacheKey(courseId: number): string {
+        return  this.ROOT_CACHE_KEY + 'chats:' + courseId;
+    }
+
+    /**
+     * Get cache key for sessions WS call.
+     *
+     * @param {number} chatId Chat ID.
+     * @param {number} groupId Goup ID, 0 means that the function will determine the user group.
+     * @param {boolean} showAll Whether to include incomplete sessions or not.
+     * @return {string} Cache key.
+     */
+    protected getSessionsCacheKey(chatId: number, groupId: number, showAll: boolean): string {
+        return  this.getSessionsCacheKeyPrefix(chatId) + groupId + ':' + (showAll ? 1 : 0);
+    }
+
+    /**
+     * Get cache key prefix for sessions WS call.
+     *
+     * @param {number} chatId Chat ID.
+     * @return {string} Cache key prefix.
+     */
+    protected getSessionsCacheKeyPrefix(chatId: number): string {
+        return  this.ROOT_CACHE_KEY + 'sessions:' + chatId + ':';
+    }
+
+    /**
+     * Get cache key for session messages WS call.
+     *
+     * @param {number} chatId Chat ID.
+     * @param {number} sessionStart Session start time.
+     * @param {number} groupId Group ID, 0 means that the function will determine the user group.
+     * @return {string} Cache key.
+     */
+    protected getSessionMessagesCacheKey(chatId: number, sessionStart: number, groupId: number): string {
+        return this.getSessionMessagesCacheKeyPrefix(chatId) + sessionStart + ':' + groupId;
+    }
+
+    /**
+     * Get cache key prefix for session messages WS call.
+     *
+     * @param {number} chatId Chat ID.
+     * @return {string} Cache key prefix.
+     */
+    protected getSessionMessagesCacheKeyPrefix(chatId: number): string {
+        return this.ROOT_CACHE_KEY + 'sessionsMessages:' + chatId + ':';
     }
 }
