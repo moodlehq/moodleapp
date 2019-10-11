e14b3ed0f8628c71aa5014d21b7832fbf02366e9
Dani Palou <dani@moodle.com>
2019-10-11 09:18:56 +0200

    MOBILE-2995 qr: Add scan QR option in more menu

--- src/core/mainmenu/pages/more/more.ts
+++ src/core/mainmenu/pages/more/more.ts
@@ -16,9 +16,13 @@ import { Component, OnDestroy } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
 import { CoreEventsProvider } from '@providers/events';
 import { CoreSitesProvider } from '@providers/sites';
+import { CoreTextUtilsProvider } from '@providers/utils/text';
+import { CoreUtilsProvider } from '@providers/utils/utils';
 import { CoreMainMenuDelegate, CoreMainMenuHandlerData } from '../../providers/delegate';
 import { CoreMainMenuProvider, CoreMainMenuCustomItem } from '../../providers/mainmenu';
 import { CoreLoginHelperProvider } from '@core/login/providers/helper';
+import { CoreContentLinksHelperProvider } from '@core/contentlinks/providers/helper';
+import { TranslateService } from '@ngx-translate/core';
 
 /**
  * Page that displays the list of main menu options that aren't in the tabs.
@@ -35,6 +39,7 @@ export class CoreMainMenuMorePage implements OnDestroy {
     siteInfo: any;
     siteName: string;
     logoutLabel: string;
+    showScanQR: boolean;
     showWeb: boolean;
     showHelp: boolean;
     docsUrl: string;
@@ -45,14 +50,22 @@ export class CoreMainMenuMorePage implements OnDestroy {
     protected langObserver;
     protected updateSiteObserver;
 
-    constructor(private menuDelegate: CoreMainMenuDelegate, private sitesProvider: CoreSitesProvider,
-            private navCtrl: NavController, private mainMenuProvider: CoreMainMenuProvider,
-            eventsProvider: CoreEventsProvider, private loginHelper: CoreLoginHelperProvider) {
+    constructor(private menuDelegate: CoreMainMenuDelegate,
+            private sitesProvider: CoreSitesProvider,
+            private navCtrl: NavController,
+            private mainMenuProvider: CoreMainMenuProvider,
+            eventsProvider: CoreEventsProvider,
+            private loginHelper: CoreLoginHelperProvider,
+            private utils: CoreUtilsProvider,
+            private linkHelper: CoreContentLinksHelperProvider,
+            private textUtils: CoreTextUtilsProvider,
+            private translate: TranslateService) {
 
         this.langObserver = eventsProvider.on(CoreEventsProvider.LANGUAGE_CHANGED, this.loadSiteInfo.bind(this));
         this.updateSiteObserver = eventsProvider.on(CoreEventsProvider.SITE_UPDATED, this.loadSiteInfo.bind(this),
             sitesProvider.getCurrentSiteId());
         this.loadSiteInfo();
+        this.showScanQR = this.utils.canScanQR();
     }
 
     /**
@@ -153,6 +166,30 @@ export class CoreMainMenuMorePage implements OnDestroy {
      */
     openSitePreferences(): void {
         this.navCtrl.push('CoreSitePreferencesPage');
+    }
+
+    /**
+     * Scan and treat a QR code.
+     */
+    scanQR(): void {
+        // Scan for a QR code.
+        this.utils.scanQR().then((text) => {
+            if (text) {
+                // Check if it's a URL. We basically check it has a protocol and doesn't include any space.
+                if (/^[^:]{2,}:\/\/[^ ]+$/i.test(text)) {
+                    // Check if the app can handle the URL.
+                    this.linkHelper.handleLink(text, undefined, this.navCtrl, true, true).then((treated) => {
+                        if (!treated) {
+                            // Can't handle it, open it in browser.
+                            this.sitesProvider.getCurrentSite().openInBrowserWithAutoLoginIfSameSite(text);
+                        }
+                    });
+                } else {
+                    // It's not a URL, open it in a modal so the user can see it and copy it.
+                    this.textUtils.expandText(this.translate.instant('core.qrscanner'), text);
+                }
+            }
+        });
     }
 
     /**
