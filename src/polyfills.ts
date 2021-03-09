0ce486ce9d04dff6d7275e0acea7a4b6885558ee
Noel De Martin <noel@moodle.com>
2021-03-09 12:53:51 +0100

    MOBILE-3720 env: Add missing polyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -17,3 +17,42 @@ window.__Zone_disable_customElements = true;
 
 // Zone JS is required by default for Angular itself.
 import 'zone.js/dist/zone';
+
+// Platform polyfills
+import 'core-js/es/array/includes';
+import 'core-js/es/promise/finally';
+import 'core-js/es/string/match-all';
+import 'core-js/es/string/trim-right';
+
+polyfillEventComposedPath();
+
+/**
+ * Polyfill Event.composedPath() if necessary.
+ *
+ * @see https://github.com/ionic-team/stencil/issues/2681
+ */
+function polyfillEventComposedPath() {
+    const event = new Event('') as { path?: NodeList };
+
+    if (!('path' in event && event.path instanceof NodeList)) {
+        return;
+    }
+
+    Event.prototype.composedPath = function () {
+        if (this._composedPath) {
+            return this._composedPath;
+        }
+
+        let node = this.target;
+
+        for (this._composedPath = []; node.parentNode !== null;) {
+            this._composedPath.push(node);
+
+            node = node.parentNode;
+        }
+
+        this._composedPath.push(document, window);
+
+        return this._composedPath;
+    };
+}
