533fe8e1b443968ab41c99bba2b984fd29530885
Pau Ferrer Ocaña <crazyserver@gmail.com>
2022-02-24 10:42:12 +0100

    MOBILE-3996 navbar: Add navbar at the bottom which disappears on scroll

--- src/core/components/navigation-bar/navigation-bar.ts
+++ src/core/components/navigation-bar/navigation-bar.ts
@@ -12,8 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Component, EventEmitter, Input, OnChanges, Output, SimpleChange } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnChanges, OnDestroy, Output, SimpleChange } from '@angular/core';
+import { IonContent } from '@ionic/angular';
+import { ScrollDetail } from '@ionic/core';
+import { CoreUtils } from '@services/utils/utils';
 import { Translate } from '@singletons';
+import { CoreMath } from '@singletons/math';
 
 /**
  * Component to show a "bar" with arrows to navigate forward/backward and an slider to move around.
@@ -29,7 +33,7 @@ import { Translate } from '@singletons';
     templateUrl: 'core-navigation-bar.html',
     styleUrls: ['navigation-bar.scss'],
 })
-export class CoreNavigationBarComponent implements OnChanges {
+export class CoreNavigationBarComponent implements OnDestroy, OnChanges {
 
     @Input() items: CoreNavigationBarItem[] = []; // List of items.
     @Input() previousTranslate = 'core.previous'; // Previous translatable text, can admit $a variable.
@@ -48,8 +52,117 @@ export class CoreNavigationBarComponent implements OnChanges {
     progress = 0;
     progressText = '';
 
+    protected element: HTMLElement;
+    protected initialHeight = 0;
+    protected initialPaddingBottom = 0;
+    protected previousTop = 0;
+    protected previousHeight = 0;
+    protected stickTimeout?: number;
+    protected content?: HTMLIonContentElement | null;
+
     // Function to call when arrow is clicked. Will receive as a param the item to load.
     @Output() action: EventEmitter<unknown> = new EventEmitter<unknown>();
+
+    constructor(el: ElementRef, protected ionContent: IonContent) {
+        this.element = el.nativeElement;
+        this.element.setAttribute('slot', 'fixed'); // Just in case somebody forgets to add it.
+    }
+
+    /**
+     * Setup scroll event listener.
+     *
+     * @param retries Number of retries left.
+     */
+    protected async listenScrollEvents(retries = 3): Promise<void> {
+        // Already initialized.
+        if (this.initialHeight > 0) {
+            return;
+        }
+
+        this.initialHeight = this.element.getBoundingClientRect().height;
+
+        if (this.initialHeight == 0 && retries > 0) {
+            await CoreUtils.nextTicks(50);
+
+            this.listenScrollEvents(retries - 1);
+
+            return;
+        }
+        // Set a minimum height value.
+        this.initialHeight = this.initialHeight || 48;
+        this.previousHeight = this.initialHeight;
+
+        this.content = this.element.closest('ion-content');
+
+        if (!this.content) {
+            return;
+        }
+
+        this.content.classList.add('has-core-navigation');
+
+        // Move element to the nearest ion-content if it's not the parent.
+        if (this.element.parentElement?.nodeName != 'ION-CONTENT') {
+            this.content.appendChild(this.element);
+        }
+
+        // Set a padding to not overlap elements.
+        this.initialPaddingBottom = parseFloat(this.content.style.getPropertyValue('--padding-bottom') || '0');
+        this.content.style.setProperty('--padding-bottom', this.initialPaddingBottom + this.initialHeight + 'px');
+        const scroll = await this.content.getScrollElement();
+        this.content.scrollEvents = true;
+
+        this.setBarHeight(this.initialHeight);
+        this.content.addEventListener('ionScroll', (e: CustomEvent<ScrollDetail>): void => {
+            if (!this.content) {
+                return;
+            }
+
+            this.onScroll(e.detail, scroll);
+        });
+
+    }
+
+    /**
+     * On scroll function.
+     *
+     * @param scrollDetail Scroll detail object.
+     * @param scrollElement Scroll element to calculate maxScroll.
+     */
+    protected onScroll(scrollDetail: ScrollDetail, scrollElement: HTMLElement): void {
+        const maxScroll = scrollElement.scrollHeight - scrollElement.offsetHeight;
+        if (scrollDetail.scrollTop <= 0 || scrollDetail.scrollTop >= maxScroll) {
+            // Reset.
+            this.setBarHeight(this.initialHeight);
+        } else {
+            let newHeight = this.previousHeight - (scrollDetail.scrollTop - this.previousTop);
+            newHeight = CoreMath.clamp(newHeight, 0, this.initialHeight);
+
+            this.setBarHeight(newHeight);
+        }
+        this.previousTop = scrollDetail.scrollTop;
+    }
+
+    /**
+     * Sets the bar height.
+     *
+     * @param height The new bar height.
+     */
+    protected setBarHeight(height: number): void {
+        if (this.stickTimeout) {
+            clearTimeout(this.stickTimeout);
+        }
+
+        this.element.style.opacity = height <= 0 ? '0' : '1';
+        this.content?.style.setProperty('--core-navigation-height', height + 'px');
+        this.previousHeight = height;
+
+        if (height > 0 && height < this.initialHeight) {
+            // Finish opening or closing the bar.
+            const newHeight = height < this.initialHeight / 2 ? 0 : this.initialHeight;
+
+            this.stickTimeout = window.setTimeout(() => this.setBarHeight(newHeight), 500);
+        }
+    }
 
     /**
      * @inheritdoc
@@ -76,6 +189,8 @@ export class CoreNavigationBarComponent implements OnChanges {
         if (this.previousIndex >= 0) {
             this.previousTitle = Translate.instant(this.previousTranslate, { $a: this.items[this.previousIndex].title || '' });
         }
+
+        this.listenScrollEvents();
     }
 
     /**
@@ -90,6 +205,13 @@ export class CoreNavigationBarComponent implements OnChanges {
 
         this.currentIndex = itemIndex;
         this.action.emit(this.items[itemIndex].item);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    async ngOnDestroy(): Promise<void> {
+        this.content?.style.setProperty('--padding-bottom', this.initialPaddingBottom + 'px');
     }
 
 }
