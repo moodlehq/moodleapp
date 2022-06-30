73b108e5c52fcb3e93b247245ac022ee2bb6a1f7
Dani Palou <dani@moodle.com>
2022-06-30 16:39:09 +0200

    MOBILE-3817 core: Implement zipIncludingComplete and add tests

--- src/core/utils/observables.ts
+++ src/core/utils/observables.ts
@@ -14,7 +14,7 @@
 
 import { CoreError } from '@classes/errors/error';
 import { CoreSubscriptions } from '@singletons/subscriptions';
-import { BehaviorSubject, Observable, of } from 'rxjs';
+import { BehaviorSubject, Observable, of, Subscription } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 /**
@@ -28,11 +28,7 @@ export function asyncObservable<T>(createObservable: () => Promise<Observable<T>
 
     return new Observable(subscriber => {
         promise
-            .then(observable => observable.subscribe(
-                value => subscriber.next(value),
-                error => subscriber.error(error),
-                () => subscriber.complete(),
-            ))
+            .then(observable => observable.subscribe(subscriber))
             .catch(error => subscriber.error(error));
     });
 }
@@ -71,4 +67,97 @@ export function ignoreErrors<Result, Fallback>(
     fallback?: Fallback,
 ): Observable<Result | Fallback | undefined> {
     return observable.pipe(catchError(() => of(fallback)));
+}
+
+/**
+ * Get return types of a list of observables.
+ */
+type GetObservablesReturnTypes<T> = { [key in keyof T]: T[key] extends Observable<infer R> ? R : never };
+
+/**
+ * Data for an observable when zipping.
+ */
+type ZipObservableData<T = any> = { // eslint-disable-line @typescript-eslint/no-explicit-any
+    values: T[];
+    completed: boolean;
+    subscription?: Subscription;
+};
+
+/**
+ * Similar to rxjs zip operator, but once an observable completes we'll continue to emit the last value as long
+ * as the other observables continue to emit values.
+ *
+ * @param observables Observables to zip.
+ * @return Observable that emits the zipped values.
+ */
+export function zipIncudingComplete<T extends Observable<any>[]>( // eslint-disable-line @typescript-eslint/no-explicit-any
+    ...observables: T
+): Observable<GetObservablesReturnTypes<T>> {
+    return new Observable(subscriber => {
+        const observablesData: ZipObservableData[] = [];
+        let nextIndex = 0;
+        let numCompleted = 0;
+        let hasErrored = false;
+
+        // Treat an emitted event.
+        const treatEmitted = (completed = false) => {
+            if (hasErrored) {
+                return;
+            }
+
+            if (numCompleted >= observables.length) {
+                subscriber.complete();
+
+                return;
+            }
+
+            // Check if any observable still doesn't have data for the index.
+            const notReady = observablesData.some(data => !data.completed && data.values[nextIndex] === undefined);
+            if (notReady) {
+                return;
+            }
+
+            // For each observable, get the value for the next index, or last value if not present (completed).
+            const valueToEmit = observablesData.map(observableData =>
+                observableData.values[nextIndex] ?? observableData.values[observableData.values.length - 1]);
+
+            nextIndex++;
+            subscriber.next(<GetObservablesReturnTypes<T>> valueToEmit);
+
+            if (completed) {
+                // An observable was completed, there might be other values to emit.
+                treatEmitted(true);
+            }
+        };
+
+        observables.forEach((observable, obsIndex) => {
+            const observableData: ZipObservableData = {
+                values: [],
+                completed: false,
+            };
+
+            observableData.subscription = observable.subscribe({
+                next: (value) => {
+                    observableData.values.push(value);
+                    treatEmitted();
+                },
+                error: (error) => {
+                    hasErrored = true;
+                    subscriber.error(error);
+                },
+                complete: () => {
+                    observableData.completed = true;
+                    numCompleted++;
+                    treatEmitted(true);
+                },
+            });
+
+            observablesData[obsIndex] = observableData;
+        });
+
+        // When unsubscribing, unsubscribe from all observables.
+        return () => {
+            observablesData.forEach(observableData => observableData.subscription?.unsubscribe());
+        };
+    });
 }
