b62825646c74bfb4e9567914341fbd083c718808
Noel De Martin <noel@moodle.com>
2023-06-12 14:53:42 +0200

    MOBILE-4362 test: Update deprecated deps usage

--- jest.config.js
+++ jest.config.js
@@ -19,7 +19,7 @@ module.exports = {
     },
     globals: {
         'ts-jest': {
-            tsConfig: './tsconfig.test.json',
+            tsconfig: './tsconfig.test.json',
         },
     },
 };
