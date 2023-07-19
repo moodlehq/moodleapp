a4edff16bb883b8ffcdc668163fd586e1fda3a7d
Pau Ferrer Ocaña <crazyserver@gmail.com>
2023-07-19 12:55:47 +0200

    MOBILE-4379 chat: Remove legacy chat time message

--- scripts/update_lang_functions.sh
+++ scripts/update_lang_functions.sh
@@ -22,7 +22,7 @@ function copy_lang {
     lang=$1
 
     index_keys=$(jq -r 'to_entries[] | "\"\(.key)\","' langindex.json)
-    index_keys=${index_keys:0:-1}
+    index_keys=${index_keys:0:${#index_keys}-1}
 
     hyphenlang=${lang/_/-}
     langfilepath=$LANG_PATH/$hyphenlang.json
