b1b8b9583bf530ad5ce33f70611d96ee1ba8ccee
Noel De Martin <noel@moodle.com>
2023-12-04 17:01:05 +0100

    MOBILE-3947 core: Fix circular routes

--- src/core/features/mainmenu/mainmenu-tab-routing.module.ts
+++ src/core/features/mainmenu/mainmenu-tab-routing.module.ts
@@ -18,24 +18,64 @@ import { Route, Routes } from '@angular/router';
 import { ModuleRoutesConfig, resolveModuleRoutes } from '@/app/app-routing.module';
 
 const MAIN_MENU_TAB_ROUTES = new InjectionToken('MAIN_MENU_TAB_ROUTES');
+const modulesPaths: Record<string, Set<string>> = {};
+
+/**
+ * Get the name of the module the injector belongs to.
+ *
+ * @param injector Injector.
+ * @returns Injector module name.
+ */
+function getInjectorModule(injector: Injector): string | null {
+    if (!('source' in injector) || typeof injector.source !== 'string') {
+        return null;
+    }
+
+    // Get module name from R3Injector source.
+    // See https://github.com/angular/angular/blob/16.2.0/packages/core/src/di/r3_injector.ts#L161C8
+    return injector.source;
+}
+
+/**
+ * Get module paths.
+ *
+ * @param injector Injector.
+ * @returns Module paths.
+ */
+function getModulePaths(injector: Injector): Set<string> | null {
+    const module = getInjectorModule(injector);
+
+    if (!module) {
+        return null;
+    }
+
+    return modulesPaths[module] ??= new Set();
+}
 
 /**
  * Build module routes.
  *
  * @param injector Injector.
+ * @param mainRoute Main route.
  * @returns Routes.
  */
 export function buildTabMainRoutes(injector: Injector, mainRoute: Route): Routes {
+    const path = mainRoute.path ?? '';
+    const modulePaths = getModulePaths(injector);
+    const isRootRoute = modulePaths && !modulePaths.has(path);
     const routes = resolveModuleRoutes(injector, MAIN_MENU_TAB_ROUTES);
 
-    mainRoute.path = mainRoute.path || '';
-    mainRoute.children = mainRoute.children || [];
-    mainRoute.children = mainRoute.children.concat(routes.children);
+    mainRoute.path = path;
+    modulePaths?.add(path);
 
-    return [
-        mainRoute,
-        ...routes.siblings,
-    ];
+    if (isRootRoute && !('redirectTo' in mainRoute)) {
+        mainRoute.children = mainRoute.children || [];
+        mainRoute.children = mainRoute.children.concat(routes.children);
+    }
+
+    return isRootRoute
+        ? [mainRoute, ...routes.siblings]
+        : [mainRoute];
 }
 
 @NgModule()
