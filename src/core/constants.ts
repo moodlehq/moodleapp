df0cf232f6bf7137d9ddd8f440cc93c0a71b20e4
Dani Palou <dani@moodle.com>
2023-12-15 10:42:31 +0100

    MOBILE-3947 ion-datetime: Use right language and first day of week

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -153,6 +153,9 @@ export class CoreConstants {
     static readonly MOD_ARCHETYPE_ASSIGNMENT = 2; // Assignment module archetype.
     static readonly MOD_ARCHETYPE_SYSTEM = 3; // System (not user-addable) module archetype.
 
+    // Other constants.
+    static readonly CALENDAR_DEFAULT_STARTING_WEEKDAY = 1;
+
     // Config & environment constants.
     static readonly CONFIG = { ...envJson.config } as unknown as EnvironmentConfig; // Data parsed from config.json files.
     static readonly BUILD = envJson.build as unknown as EnvironmentBuild; // Build info.
