12ae4c75296be6ad7398030bcf25f84c44158846
Noel De Martin <noel@moodle.com>
2024-05-06 12:35:34 +0200

    MOBILE-4470 npm: Update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -84,16 +84,7 @@ const appConfig = {
                 },
             },
         ],
-        '@typescript-eslint/member-ordering': [
-            'error',
-            {
-                default:
-                {
-                    order: 'as-written',
-                },
-
-            }
-        ],
+        '@typescript-eslint/member-ordering': 'off',
         '@typescript-eslint/naming-convention': [
             'error',
             {
