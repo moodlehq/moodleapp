ec8a1058ddb9806cbda51a08a38c7fe0a1bb9f62
Noel De Martin <noel@moodle.com>
2024-07-03 13:06:10 +0200

    MOBILE-4616: Update supported versions

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -20,16 +20,8 @@ import 'zone.js';
 
 // Platform polyfills
 import 'core-js/es/array/at';
-import 'core-js/es/array/flat-map';
-import 'core-js/es/array/flat';
-import 'core-js/es/array/includes';
-import 'core-js/es/global-this';
-import 'core-js/es/object/from-entries';
 import 'core-js/es/object/has-own';
-import 'core-js/es/promise/finally';
 import 'core-js/es/string/at';
-import 'core-js/es/string/match-all';
-import 'core-js/es/string/trim-right';
 import 'core-js/es/typed-array/at';
 
 polyfillEventComposedPath();
