b4e8d814635b9a46528de6c432d36ef83cac32bb
Pau Ferrer Ocaña <crazyserver@gmail.com>
2024-08-16 09:00:03 +0200

    MOBILE-4616 chore: Add Moodle 4.5 release temporal number

--- src/core/classes/sites/authenticated-site.ts
+++ src/core/classes/sites/authenticated-site.ts
@@ -73,6 +73,7 @@ export class CoreAuthenticatedSite extends CoreUnauthenticatedSite {
         '4.2': 2023042400,
         '4.3': 2023100900,
         '4.4': 2024042200,
+        '4.5': 2024081600, // @TODO correct the final release date.
     };
 
     // Possible cache update frequencies.
