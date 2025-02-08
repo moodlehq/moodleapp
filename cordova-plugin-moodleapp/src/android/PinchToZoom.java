b4e4d76dd83b0b4aada94e65310ce7c4dd89e576
Vimal Prakash <developer.05@ictkerala.org>
2025-02-08 23:40:56 +0530

    ml added

--- cordova-plugin-moodleapp/src/android/PinchToZoom.java
+++ cordova-plugin-moodleapp/src/android/PinchToZoom.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.moodle.moodlemobile;
+package com.ictkerala.sec;
 
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
