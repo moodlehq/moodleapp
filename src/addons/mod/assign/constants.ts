6e7b6c92cf91540011a7560bd9801c83fdffc841
Pau Ferrer Ocaña <crazyserver@gmail.com>
2025-02-19 09:15:08 +0100

    MOBILE-3671 assign: Remove ASSIGN_ATTEMPT_REOPEN_METHOD_NONE usage
    Deprecated on MDL-80741

--- src/addons/mod/assign/constants.ts
+++ src/addons/mod/assign/constants.ts
@@ -72,8 +72,8 @@ export const enum AddonModAssignGradingStates {
  * Constants on LMS starting with ASSIGN_ATTEMPT_REOPEN_METHOD_
  */
 export const enum AddonModAssignAttemptReopenMethodValues {
-    NONE = 'none',
     MANUAL = 'manual',
+    AUTOMATIC = 'automatic',
     UNTILPASS = 'untilpass',
 }
 
