d989680851195ffd0f5b8057289624c9722d6baf
Pau Ferrer Ocaña <crazyserver@gmail.com>
2025-06-06 16:17:59 +0200

    MOBILE-4820 viewer: Image editor on avatar change

--- src/core/services/file.ts
+++ src/core/services/file.ts
@@ -606,7 +606,7 @@ export class CoreFileProvider {
         // Create file (and parent folders) to prevent errors.
         const fileEntry = await this.createFile(path);
 
-        if (this.isHTMLAPI && (typeof data == 'string' || data.toString() == '[object ArrayBuffer]')) {
+        if (this.isHTMLAPI && (typeof data === 'string' || data.toString() === '[object ArrayBuffer]')) {
             // We need to write Blobs.
             const extension = CoreMimetype.getFileExtension(path);
             const type = extension ? CoreMimetype.getMimeType(extension) : '';
