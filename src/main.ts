f678fdb7d9f1705f978aa4bec60c470ab0a1c99d
Dani Palou <dani@moodle.com>
2025-06-06 09:15:47 +0200

    MOBILE-4739 core: Enable input signals in modals

--- src/main.ts
+++ src/main.ts
@@ -53,6 +53,7 @@ bootstrapApplication(AppComponent, {
                 navAnimation: moodleTransitionAnimation,
                 innerHTMLTemplatesEnabled: true,
                 sanitizerEnabled: true,
+                useSetInputAPI: true,
             }),
             TranslateModule.forRoot({
                 loader: {
