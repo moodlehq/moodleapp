e4c6c7aef5179dc5998eb35e4c8246bda62c2df7
Alfonso <alfonsomtb98@gmail.com>
2025-08-07 14:45:19 +0200

    MOBILE-4862 opener.ts: Handle error when open file

---
+++ src/core/features/native/constants.ts
@@ -0,0 +1,26 @@
+// (C) Copyright 2015 Moodle Pty Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+export const enum CoreNativeCordovaPluginResultStatus {
+    NO_RESULT,
+    OK,
+    CLASS_NOT_FOUND_EXCEPTION,
+    ILLEGAL_ACCESS_EXCEPTION,
+    INSTANTIATION_EXCEPTION,
+    MALFORMED_URL_EXCEPTION,
+    IO_EXCEPTION,
+    INVALID_ACTION,
+    JSON_EXCEPTION,
+    ERROR
+}
