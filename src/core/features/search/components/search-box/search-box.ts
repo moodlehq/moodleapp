jibuapprepo/JibuAppAfrica

MOBILE-4842 chore: Use signals on search box component

src/core/features/search/components/search-box/search-box.ts
@@ -12,7 +12,19 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Component, Input, Output, EventEmitter, OnInit, signal, ViewChild, ElementRef } from '@angular/core';
+import {
+    Component,
+    Output,
+    EventEmitter,
+    OnInit,
+    signal,
+    ElementRef,
+    input,
+    viewChild,
+    model,
+    effect,
+    computed,
+} from '@angular/core';
 
 import { CoreSites } from '@services/sites';
 import { CoreSearchHistory } from '../../services/search-history.service';
@@ -29,8 +41,8 @@ import { CoreSharedModule } from '@/core/shared.module';
  * This component will display a standalone search box with its search button in order to have a better UX.
  *
  * Example usage:
- * <core-search-box (onSubmit)="search($event)" [placeholder]="'core.courses.search' | translate"
- *     [searchLabel]="'core.courses.search' | translate" autoFocus="true"></core-search-box>
+ * <core-search-box (searchTextChange)="search($event)" [placeholder]="'core.courses.search' | translate"
+ *     [searchLabel]="'core.courses.search' | translate" [autoFocus]="true" />
  */
 @Component({
     selector: 'core-search-box',
@@ -42,43 +54,64 @@ import { CoreSharedModule } from '@/core/shared.module';
 })
 export class CoreSearchBoxComponent implements OnInit {
 
-    @Input() searchLabel?: string; // Label to be used on action button.
-    @Input() placeholder?: string; // Placeholder text for search text input.
-    @Input() autocorrect = 'on'; // Enables/disable Autocorrection on search text input.
-    @Input({ transform: toBoolean }) spellcheck = true; // Enables/disable Spellchecker on search text input.
-    @Input({ transform: toBoolean }) autoFocus = false; // Enables/disable Autofocus when entering view.
-    @Input() lengthCheck = 3; // Check value length before submit. If 0, any string will be submitted.
-    @Input({ transform: toBoolean }) showClear = true; // Show/hide clear button.
-    @Input({ transform: toBoolean }) disabled = false; // Disables the input text.
-    @Input() initialSearch = ''; // Initial search text.
+    readonly searchLabel = input(Translate.instant('core.search')); // Label to be used on action button.
+    readonly placeholder = input(Translate.instant('core.search')); // Placeholder text for search text input.
+    readonly autocorrect = input<'on'|'off'>('on'); // Enables/disable Autocorrection on search text input.
+    readonly spellcheck = input(true, { transform: toBoolean }); // Enables/disable Spellchecker on search text input.
+    readonly autoFocus = input(false, { transform: toBoolean }); // Enables/disable Autofocus when entering view.
+    readonly lengthCheck = input(3); // Check value length before submit. If 0, any string will be submitted.
+    readonly showClear = input(true, { transform: toBoolean }); // Show/hide clear button.
+    readonly disabled = input(false, { transform: toBoolean }); // Disables the input text.
+    /**
+     * @deprecated since 5.1. Use (searchText) instead.
+     */
+    readonly initialSearch = input(''); // Initial search text.
 
+    // eslint-disable-next-line @typescript-eslint/no-deprecated
+    readonly searchText = model(this.initialSearch());
     /* If provided. It will save and display a history of searches for this particular Id.
      * To use different history lists, place different Id.
      * I.e. AddonMessagesContacts or CoreUserParticipants-6 (using the course Id).*/
-    @Input() searchArea = '';
+    readonly searchArea = input('');
 
-    @Output() onSubmit: EventEmitter<string>; // Send data when submitting the search form.
-    @Output() onClear: EventEmitter<void>; // Send event when clearing the search form.
+    /**
+     * @deprecated since 5.1. Use (searchTextChange) instead.
+     */
+    @Output() onSubmit = new EventEmitter<string>(); // Send data when submitting the search form.
+    /**
+     * @deprecated since 5.1. Use (searchTextChange) instead.
+     */
+    @Output() onClear = new EventEmitter<void>(); // Send event when clearing the search form.
 
-    @ViewChild('searchForm') formElement?: ElementRef;
-    searched = ''; // Last search emitted.
-    searchText = '';
-    history: CoreSearchHistoryDBRecord[] = [];
+    readonly formElement = viewChild<ElementRef<HTMLFormElement>>('searchForm');
+    readonly textInput = signal(''); // Text input value.
+    readonly history = signal<CoreSearchHistoryDBRecord[]>([]);
     readonly historyShown = signal(false);
     readonly showLengthAlert = signal(false);
+    readonly dirty = computed(() =>
+        // If the text input is dirty, it means that the user has changed the text input.
+        this.textInput() !== this.searchText());
 
     constructor() {
-        this.onSubmit = new EventEmitter<string>();
-        this.onClear = new EventEmitter<void>();
+        // Change the text input value when the searchText from outside of the component changes.
+        effect(() => {
+            const newText = this.searchText();
+            this.textInput.set(newText);
+        });
     }
 
+    /**
+     * @inheritdoc
+     */
     ngOnInit(): void {
-        this.searchLabel = this.searchLabel || Translate.instant('core.search');
-        this.placeholder = this.placeholder || Translate.instant('core.search');
-        this.searchText = this.initialSearch;
-
-        if (this.searchArea) {
+        if (this.searchArea()) {
             this.loadHistory();
+        }
+
+        // Perform an initial search.
+        if (this.searchText()) {
+            this.textInput.set(this.searchText());
+            this.submitForm();
         }
     }
 
@@ -91,7 +124,8 @@ export class CoreSearchBoxComponent implements OnInit {
         e?.preventDefault();
         e?.stopPropagation();
 
-        if (this.searchText.length < this.lengthCheck) {
+        const textInput = this.textInput();
+        if (textInput.length < this.lengthCheck()) {
             this.showLengthAlert.set(true);
 
             return;
@@ -99,26 +133,27 @@ export class CoreSearchBoxComponent implements OnInit {
 
         this.showLengthAlert.set(false);
 
-        if (this.searchArea) {
-            this.saveSearchToHistory(this.searchText);
+        if (this.searchArea()) {
+            this.saveSearchToHistory(textInput);
         }
 
-        CoreForms.triggerFormSubmittedEvent(this.formElement, false, CoreSites.getCurrentSiteId());
+        CoreForms.triggerFormSubmittedEvent(this.formElement(), false, CoreSites.getCurrentSiteId());
 
         this.historyShown.set(false);
-        this.searched = this.searchText;
-        this.onSubmit.emit(this.searchText);
+        this.searchText.set(textInput);
+
+        // eslint-disable-next-line @typescript-eslint/no-deprecated
+        this.onSubmit.emit(textInput);
     }
 
     /**
      * Saves the search term onto the history.
      *
      * @param text Text to save.
-     * @returns Promise resolved when done.
      */
     protected async saveSearchToHistory(text: string): Promise<void> {
         try {
-            await CoreSearchHistory.insertOrUpdateSearchText(this.searchArea, text.toLowerCase());
+            await CoreSearchHistory.insertOrUpdateSearchText(this.searchArea(), text.toLowerCase());
         } finally {
             this.loadHistory();
         }
@@ -126,11 +161,11 @@ export class CoreSearchBoxComponent implements OnInit {
 
     /**
      * Loads search history.
-     *
-     * @returns Promise resolved when done.
      */
     protected async loadHistory(): Promise<void> {
-        this.history = await CoreSearchHistory.getSearchHistory(this.searchArea);
+        const history = await CoreSearchHistory.getSearchHistory(this.searchArea());
+
+        this.history.set(history);
     }
 
     /**
@@ -140,8 +175,8 @@ export class CoreSearchBoxComponent implements OnInit {
      * @param text Selected text.
      */
     historyClicked(e: Event, text: string): void {
-        if (this.searched != text) {
-            this.searchText = text;
+        if (this.textInput() !== text) {
+            this.textInput.set(text);
             this.submitForm(e);
         }
     }
@@ -150,9 +185,11 @@ export class CoreSearchBoxComponent implements OnInit {
      * Form submitted.
      */
     clearForm(): void {
-        this.searched = '';
-        this.searchText = '';
+        this.textInput.set('');
+        this.searchText.set('');
         this.showLengthAlert.set(false);
+
+        // eslint-disable-next-line @typescript-eslint/no-deprecated
         this.onClear.emit();
     }
 
@@ -169,7 +206,7 @@ export class CoreSearchBoxComponent implements OnInit {
     checkFocus(): void {
         // Wait until the new element is focused.
         setTimeout(() => {
-            if (document.activeElement?.closest('form') !== this.formElement?.nativeElement) {
+            if (document.activeElement?.closest('form') !== this.formElement()?.nativeElement) {
                 this.historyShown.set(false);
                 this.showLengthAlert.set(false);
             }

moodlehq/moodleapp

src/core/features/search/components/search-box/search-box.ts
@@ -12,7 +12,19 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Component, Input, Output, EventEmitter, OnInit, signal, ViewChild, ElementRef } from '@angular/core';
+import {
+    Component,
+    Output,
+    EventEmitter,
+    OnInit,
+    signal,
+    ElementRef,
+    input,
+    viewChild,
+    model,
+    effect,
+    computed,
+} from '@angular/core';
 
 import { CoreSites } from '@services/sites';
 import { CoreSearchHistory } from '../../services/search-history.service';
@@ -29,8 +41,8 @@ import { CoreSharedModule } from '@/core/shared.module';
  * This component will display a standalone search box with its search button in order to have a better UX.
  *
  * Example usage:
- * <core-search-box (onSubmit)="search($event)" [placeholder]="'core.courses.search' | translate"
- *     [searchLabel]="'core.courses.search' | translate" autoFocus="true"></core-search-box>
+ * <core-search-box (searchTextChange)="search($event)" [placeholder]="'core.courses.search' | translate"
+ *     [searchLabel]="'core.courses.search' | translate" [autoFocus]="true" />
  */
 @Component({
     selector: 'core-search-box',
@@ -42,43 +54,73 @@ import { CoreSharedModule } from '@/core/shared.module';
 })
 export class CoreSearchBoxComponent implements OnInit {
 
-    @Input() searchLabel?: string; // Label to be used on action button.
-    @Input() placeholder?: string; // Placeholder text for search text input.
-    @Input() autocorrect = 'on'; // Enables/disable Autocorrection on search text input.
-    @Input({ transform: toBoolean }) spellcheck = true; // Enables/disable Spellchecker on search text input.
-    @Input({ transform: toBoolean }) autoFocus = false; // Enables/disable Autofocus when entering view.
-    @Input() lengthCheck = 3; // Check value length before submit. If 0, any string will be submitted.
-    @Input({ transform: toBoolean }) showClear = true; // Show/hide clear button.
-    @Input({ transform: toBoolean }) disabled = false; // Disables the input text.
-    @Input() initialSearch = ''; // Initial search text.
+    readonly searchLabel = input(Translate.instant('core.search')); // Label to be used on action button.
+    readonly placeholder = input(Translate.instant('core.search')); // Placeholder text for search text input.
+    readonly autocorrect = input<'on'|'off'>('on'); // Enables/disable Autocorrection on search text input.
+    readonly spellcheck = input(true, { transform: toBoolean }); // Enables/disable Spellchecker on search text input.
+    readonly autoFocus = input(false, { transform: toBoolean }); // Enables/disable Autofocus when entering view.
+    readonly lengthCheck = input(3); // Check value length before submit. If 0, any string will be submitted.
+    readonly showClear = input(true, { transform: toBoolean }); // Show/hide clear button.
+    readonly disabled = input(false, { transform: toBoolean }); // Disables the input text.
+    /**
+     * @deprecated since 5.1. Use [searchText] instead.
+     * Beware changing searchText will change the value of the initialSearch while initialSearch changes didn't affect the value.
+     */
+    readonly initialSearch = input(''); // Initial search text.
 
-    /* If provided. It will save and display a history of searches for this particular Id.
+    /**
+     * Search text. The change event will be emitted only when the submit or the clear button are pressed.
+     */
+    // eslint-disable-next-line @typescript-eslint/no-deprecated
+    readonly searchText = model(this.initialSearch());
+    /**
+     * If provided. It will save and display a history of searches for this particular Id.
      * To use different history lists, place different Id.
-     * I.e. AddonMessagesContacts or CoreUserParticipants-6 (using the course Id).*/
-    @Input() searchArea = '';
+     * I.e. AddonMessagesContacts or CoreUserParticipants-6 (using the course Id).
+     */
+    readonly searchArea = input('');
 
-    @Output() onSubmit: EventEmitter<string>; // Send data when submitting the search form.
-    @Output() onClear: EventEmitter<void>; // Send event when clearing the search form.
+    /**
+     * @deprecated since 5.1. Use (searchTextChange) instead.
+     */
+    @Output() onSubmit = new EventEmitter<string>(); // Send data when submitting the search form.
+    /**
+     * @deprecated since 5.1. Use (searchTextChange) instead.
+     */
+    @Output() onClear = new EventEmitter<void>(); // Send event when clearing the search form.
 
-    @ViewChild('searchForm') formElement?: ElementRef;
-    searched = ''; // Last search emitted.
-    searchText = '';
-    history: CoreSearchHistoryDBRecord[] = [];
+    readonly formElement = viewChild<ElementRef<HTMLFormElement>>('searchForm');
+    /**
+     * Value of the input field. The value will be emitted when the form is submitted.
+     */
+    readonly inputValue = signal('');
+    readonly history = signal<CoreSearchHistoryDBRecord[]>([]);
     readonly historyShown = signal(false);
     readonly showLengthAlert = signal(false);
+    readonly dirty = computed(() =>
+        // If the text input is dirty, it means that the user has changed the text input.
+        this.inputValue() !== this.searchText());
 
     constructor() {
-        this.onSubmit = new EventEmitter<string>();
-        this.onClear = new EventEmitter<void>();
+        // Change the text input value when the searchText from outside of the component changes.
+        effect(() => {
+            const newText = this.searchText();
+            this.inputValue.set(newText);
+        });
     }
 
+    /**
+     * @inheritdoc
+     */
     ngOnInit(): void {
-        this.searchLabel = this.searchLabel || Translate.instant('core.search');
-        this.placeholder = this.placeholder || Translate.instant('core.search');
-        this.searchText = this.initialSearch;
-
-        if (this.searchArea) {
+        if (this.searchArea()) {
             this.loadHistory();
+        }
+
+        // Perform an initial search.
+        if (this.searchText()) {
+            this.inputValue.set(this.searchText());
+            this.submitForm();
         }
     }
 
@@ -91,7 +133,8 @@ export class CoreSearchBoxComponent implements OnInit {
         e?.preventDefault();
         e?.stopPropagation();
 
-        if (this.searchText.length < this.lengthCheck) {
+        const textInput = this.inputValue();
+        if (textInput.length < this.lengthCheck()) {
             this.showLengthAlert.set(true);
 
             return;
@@ -99,26 +142,27 @@ export class CoreSearchBoxComponent implements OnInit {
 
         this.showLengthAlert.set(false);
 
-        if (this.searchArea) {
-            this.saveSearchToHistory(this.searchText);
+        if (this.searchArea()) {
+            this.saveSearchToHistory(textInput);
         }
 
-        CoreForms.triggerFormSubmittedEvent(this.formElement, false, CoreSites.getCurrentSiteId());
+        CoreForms.triggerFormSubmittedEvent(this.formElement(), false, CoreSites.getCurrentSiteId());
 
         this.historyShown.set(false);
-        this.searched = this.searchText;
-        this.onSubmit.emit(this.searchText);
+        this.searchText.set(textInput);
+
+        // eslint-disable-next-line @typescript-eslint/no-deprecated
+        this.onSubmit.emit(textInput);
     }
 
     /**
      * Saves the search term onto the history.
      *
      * @param text Text to save.
-     * @returns Promise resolved when done.
      */
     protected async saveSearchToHistory(text: string): Promise<void> {
         try {
-            await CoreSearchHistory.insertOrUpdateSearchText(this.searchArea, text.toLowerCase());
+            await CoreSearchHistory.insertOrUpdateSearchText(this.searchArea(), text.toLowerCase());
         } finally {
             this.loadHistory();
         }
@@ -126,11 +170,11 @@ export class CoreSearchBoxComponent implements OnInit {
 
     /**
      * Loads search history.
-     *
-     * @returns Promise resolved when done.
      */
     protected async loadHistory(): Promise<void> {
-        this.history = await CoreSearchHistory.getSearchHistory(this.searchArea);
+        const history = await CoreSearchHistory.getSearchHistory(this.searchArea());
+
+        this.history.set(history);
     }
 
     /**
@@ -140,8 +184,8 @@ export class CoreSearchBoxComponent implements OnInit {
      * @param text Selected text.
      */
     historyClicked(e: Event, text: string): void {
-        if (this.searched != text) {
-            this.searchText = text;
+        if (this.inputValue() !== text) {
+            this.inputValue.set(text);
             this.submitForm(e);
         }
     }
@@ -150,9 +194,11 @@ export class CoreSearchBoxComponent implements OnInit {
      * Form submitted.
      */
     clearForm(): void {
-        this.searched = '';
-        this.searchText = '';
+        this.inputValue.set('');
+        this.searchText.set('');
         this.showLengthAlert.set(false);
+
+        // eslint-disable-next-line @typescript-eslint/no-deprecated
         this.onClear.emit();
     }
 
@@ -169,7 +215,7 @@ export class CoreSearchBoxComponent implements OnInit {
     checkFocus(): void {
         // Wait until the new element is focused.
         setTimeout(() => {
-            if (document.activeElement?.closest('form') !== this.formElement?.nativeElement) {
+            if (document.activeElement?.closest('form') !== this.formElement()?.nativeElement) {
                 this.historyShown.set(false);
                 this.showLengthAlert.set(false);
             }