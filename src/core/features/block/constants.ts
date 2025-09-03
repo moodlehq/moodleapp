jibuapprepo/JibuAppAfrica

MOBILE-4842 block: Add block name constants

src/core/features/block/constants.ts
@@ -1,0 +1,27 @@
+// (C) Copyright 2015 Moodle Pty Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+export enum CoreBlocksRegion {
+    CONTENT = 'content',
+    MAIN = 'main',
+    SIDE = 'side', // Used as a prefix for side blocks. (site-pre, side-post).
+    FORCED = 'forced', // Special region for blocks that are always shown. See usage in app.
+}
+
+export const CORE_BLOCKS_DASHBOARD_FALLBACK_MYOVERVIEW_BLOCK = 'myoverview';
+
+export const CORE_BLOCKS_DASHBOARD_FALLBACK_BLOCKS = [
+    CORE_BLOCKS_DASHBOARD_FALLBACK_MYOVERVIEW_BLOCK,
+    'timeline',
+];

src/core/features/courses/pages/dashboard/dashboard.ts
@@ -29,6 +29,7 @@ import { CorePromiseUtils } from '@singletons/promise-utils';
 import { CoreAlerts } from '@services/overlays/alerts';
 import { CoreBlockSideBlocksButtonComponent } from '../../../block/components/side-blocks-button/side-blocks-button';
 import { CoreSharedModule } from '@/core/shared.module';
+import { CORE_BLOCKS_DASHBOARD_FALLBACK_BLOCKS } from '@features/block/constants';
 
 /**
  * Page that displays the dashboard page.
@@ -133,18 +134,13 @@ export default class CoreCoursesDashboardPage implements OnInit, OnDestroy {
      * Load fallback blocks to shown before 3.6 when dashboard blocks are not supported.
      */
     protected loadFallbackBlocks(): void {
-        this.blocks = [
-            {
-                name: 'myoverview',
-                visible: true,
-            },
-            {
-                name: 'timeline',
-                visible: true,
-            },
-        ];
+        this.blocks = CORE_BLOCKS_DASHBOARD_FALLBACK_BLOCKS.map((blockName) => ({
+            name: blockName,
+            visible: true,
+        }));
 
-        this.hasMainBlocks = CoreBlockDelegate.isBlockSupported('myoverview') || CoreBlockDelegate.isBlockSupported('timeline');
+        this.hasSideBlocks = CORE_BLOCKS_DASHBOARD_FALLBACK_BLOCKS.some((blockName) =>
+            CoreBlockDelegate.isBlockSupported(blockName));
     }
 
     /**

moodlehq/moodleapp

src/core/features/block/constants.ts
@@ -1,0 +1,27 @@
+// (C) Copyright 2015 Moodle Pty Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+export enum CoreBlocksRegion {
+    CONTENT = 'content',
+    MAIN = 'main',
+    SIDE = 'side', // Used as a prefix for side blocks. (side-pre, side-post).
+    FORCED = 'forced', // Special region for blocks that are always shown. See usage in app.
+}
+
+export const CORE_BLOCKS_DASHBOARD_FALLBACK_MYOVERVIEW_BLOCK = 'myoverview';
+
+export const CORE_BLOCKS_DASHBOARD_FALLBACK_BLOCKS = [
+    CORE_BLOCKS_DASHBOARD_FALLBACK_MYOVERVIEW_BLOCK,
+    'timeline',
+];

src/core/features/courses/pages/dashboard/dashboard.ts
@@ -29,6 +29,7 @@ import { CorePromiseUtils } from '@singletons/promise-utils';
 import { CoreAlerts } from '@services/overlays/alerts';
 import { CoreBlockSideBlocksButtonComponent } from '../../../block/components/side-blocks-button/side-blocks-button';
 import { CoreSharedModule } from '@/core/shared.module';
+import { CORE_BLOCKS_DASHBOARD_FALLBACK_BLOCKS } from '@features/block/constants';
 
 /**
  * Page that displays the dashboard page.
@@ -133,18 +134,13 @@ export default class CoreCoursesDashboardPage implements OnInit, OnDestroy {
      * Load fallback blocks to shown before 3.6 when dashboard blocks are not supported.
      */
     protected loadFallbackBlocks(): void {
-        this.blocks = [
-            {
-                name: 'myoverview',
-                visible: true,
-            },
-            {
-                name: 'timeline',
-                visible: true,
-            },
-        ];
+        this.blocks = CORE_BLOCKS_DASHBOARD_FALLBACK_BLOCKS.map((blockName) => ({
+            name: blockName,
+            visible: true,
+        }));
 
-        this.hasMainBlocks = CoreBlockDelegate.isBlockSupported('myoverview') || CoreBlockDelegate.isBlockSupported('timeline');
+        this.hasMainBlocks = CORE_BLOCKS_DASHBOARD_FALLBACK_BLOCKS.some((blockName) =>
+            CoreBlockDelegate.isBlockSupported(blockName));
     }
 
     /**