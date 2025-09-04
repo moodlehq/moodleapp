2eb5eb28ac330844782377e3215423b78b631996
Dani Palou <dani@moodle.com>
2025-09-04 12:31:21 +0200

    MOBILE-4868 popovers: Fix popover content outside of screen

--- src/core/services/overlays/popovers.ts
+++ src/core/services/overlays/popovers.ts
@@ -52,7 +52,49 @@ export class CorePopoversService {
 
         fixOverlayAriaHidden(popover);
 
+        this.fixPopoverOutsideScreen(popover, options);
+
         return popover;
+    }
+
+    /**
+     * Adjust size of the popover in case it's out of the screen. This could be solved in Ionic in the future.
+     * The changes will be applied after rendering the popover, so the popover can move after being shown.
+     *
+     * @param popover The popover element.
+     * @param options Popover options.
+     */
+    protected fixPopoverOutsideScreen(popover: HTMLIonPopoverElement, options: PopoverOptions): void {
+        const popoverContent = popover.shadowRoot?.querySelector('.popover-content');
+        if (!popoverContent) {
+            return;
+        }
+
+        const style = getComputedStyle(popoverContent);
+        const contentHeight = parseInt(style.height, 10);
+        const isTopPopover = options.side === 'top';
+        const overflowHeight = isTopPopover ?
+            ((parseInt(style.bottom, 10) + contentHeight) - popover.clientHeight) :
+            ((parseInt(style.top, 10) + contentHeight) - popover.clientHeight);
+
+        if (overflowHeight <= 1) {
+            // No overflow. Add a 1px error margin because Ionic's positioning sometimes causes a 1px overflow, and adding
+            // scroll in that case looks weird.
+            return;
+        }
+
+        const visibleHeight = contentHeight - overflowHeight;
+        if (visibleHeight < 250) {
+            // The visible height is too small, move the popover to show it fully.
+            const offset = isTopPopover ? overflowHeight : -overflowHeight;
+            popover.style.setProperty('--offset-y', `${offset}px`);
+        } else {
+            // Add a max height to allow scrolling inside the popover. Add 4px margin to leave some space with the edge.
+            popover.style.setProperty('--max-height', `${visibleHeight - 4}px`);
+            if (isTopPopover) {
+                popover.style.setProperty('--offset-y', `${Math.abs(contentHeight) + 4}px`);
+            }
+        }
     }
 
 }
