ff78c06726dc175a2e71938cec7585945dc70d84
Pau Ferrer Ocaña <crazyserver@gmail.com>
2025-10-16 15:17:08 +0200

    MOBILE-4842 navigator: Override queryParams after getting stored params

--- src/addons/messages/pages/group-conversations/group-conversations.ts
+++ src/addons/messages/pages/group-conversations/group-conversations.ts
@@ -314,10 +314,10 @@ export default class AddonMessagesGroupConversationsPage implements OnInit, OnDe
      * @inheritdoc
      */
     async ngOnInit(): Promise<void> {
-        this.route.queryParams.subscribe(async (params) => {
+        this.route.queryParams.subscribe(async (queryParams) => {
             // When a child page loads this callback is triggered too.
-            const conversationId = CoreNavigator.getRouteNumberParam('conversationId', { params });
-            const userId = CoreNavigator.getRouteNumberParam('userId', { params });
+            const conversationId = CoreNavigator.getRouteNumberParam('conversationId', { queryParams });
+            const userId = CoreNavigator.getRouteNumberParam('userId', { queryParams });
             if (conversationId || userId) {
                 // Update the selected ones.
                 this.selectedConversationId = conversationId;
