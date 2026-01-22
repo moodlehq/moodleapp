317b890f36d5c6bc8462a05c4a2639df913193be
Ali Abdelaal <ali.abdelaal2054@gmail.com>
2026-01-22 20:51:45 +0000

    Create @moodlehq+cordova-plugin-inappbrowser+5.0.0-moodle.3.patch

--- src/core/features/mainmenu/components/user-menu/user-menu.ts
+++ src/core/features/mainmenu/components/user-menu/user-menu.ts
@@ -91,8 +91,8 @@ export class CoreMainMenuUserMenuComponent implements OnInit, OnDestroy {
 
     // App version info (auto-updated by post-commit hook)
     appVersion = CoreConstants.CONFIG.versionname;
-    buildNumber = 13;
-    buildTime = '2026-01-22 19:23';
+    buildNumber = 14;
+    buildTime = '2026-01-22 20:51';
 
     // Secret debug menu (tap build number 7 times)
     debugTapCount = 0;
