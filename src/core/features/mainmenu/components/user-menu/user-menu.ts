b4b244fa0a27a35946e377e6173828283c1f047e
Ali Abdelaal <ali.abdelaal2054@gmail.com>
2026-01-23 22:04:32 +0000

    chore: update moodle.config.json version to 5.1.0

--- src/core/features/mainmenu/components/user-menu/user-menu.ts
+++ src/core/features/mainmenu/components/user-menu/user-menu.ts
@@ -93,8 +93,8 @@ export class CoreMainMenuUserMenuComponent implements OnInit, OnDestroy {
 
     // App version info (auto-updated by post-commit hook)
     appVersion = CoreConstants.CONFIG.versionname;
-    buildNumber = 71;
-    buildTime = '2026-01-23 20:41';
+    buildNumber = 72;
+    buildTime = '2026-01-23 22:04';
 
     // Secret debug menu (tap build number 7 times)
     debugTapCount = 0;
