a3af39127e2c2622ce47c84a1d112c7ce4ad10d9
Ali Abdelaal <ali.abdelaal2054@gmail.com>
2026-01-23 22:15:16 +0000

    Delete @moodlehq+phonegap-plugin-push+4.0.0-moodle.10.patch

--- src/core/features/mainmenu/components/user-menu/user-menu.ts
+++ src/core/features/mainmenu/components/user-menu/user-menu.ts
@@ -93,8 +93,8 @@ export class CoreMainMenuUserMenuComponent implements OnInit, OnDestroy {
 
     // App version info (auto-updated by post-commit hook)
     appVersion = CoreConstants.CONFIG.versionname;
-    buildNumber = 75;
-    buildTime = '2026-01-23 22:11';
+    buildNumber = 76;
+    buildTime = '2026-01-23 22:15';
 
     // Secret debug menu (tap build number 7 times)
     debugTapCount = 0;
