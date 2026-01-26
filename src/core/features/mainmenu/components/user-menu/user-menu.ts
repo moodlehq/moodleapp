d5af678a2de33aefdf791bdf7931c262ce3aaf0d
Ali Abdelaal <ali.abdelaal2054@gmail.com>
2026-01-26 22:42:18 +0000

    chore: trigger rebuild

--- src/core/features/mainmenu/components/user-menu/user-menu.ts
+++ src/core/features/mainmenu/components/user-menu/user-menu.ts
@@ -93,8 +93,8 @@ export class CoreMainMenuUserMenuComponent implements OnInit, OnDestroy {
 
     // App version info (auto-updated by post-commit hook)
     appVersion = CoreConstants.CONFIG.versionname;
-    buildNumber = 108;
-    buildTime = '2026-01-26 22:40';
+    buildNumber = 109;
+    buildTime = '2026-01-26 22:42';
 
     // Secret debug menu (tap build number 7 times)
     debugTapCount = 0;
