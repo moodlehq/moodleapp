21b02cbad71c0dc9d3667b2204c0ac6f143b23b6
Ali Abdelaal <ali.abdelaal2054@gmail.com>
2026-01-26 21:53:33 +0000

    chore: bump version to 5.1.2 (51200)

--- src/core/features/mainmenu/components/user-menu/user-menu.ts
+++ src/core/features/mainmenu/components/user-menu/user-menu.ts
@@ -93,8 +93,8 @@ export class CoreMainMenuUserMenuComponent implements OnInit, OnDestroy {
 
     // App version info (auto-updated by post-commit hook)
     appVersion = CoreConstants.CONFIG.versionname;
-    buildNumber = 101;
-    buildTime = '2026-01-26 20:50';
+    buildNumber = 102;
+    buildTime = '2026-01-26 21:53';
 
     // Secret debug menu (tap build number 7 times)
     debugTapCount = 0;
