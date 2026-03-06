1d5219e8b5bac942928c7e8b1f463423f5fe1850
Dani Palou <dani@moodle.com>
2026-03-06 09:00:25 +0100

    MOBILE-5018 settings: Allow adding new page items in dev page

--- src/core/features/settings/pages/dev/dev.ts
+++ src/core/features/settings/pages/dev/dev.ts
@@ -19,7 +19,7 @@ import {
     ALWAYS_SHOW_LOGIN_FORM_CHANGED,
     FAQ_QRCODE_INFO_DONE,
 } from '@features/login/constants';
-import { CoreSettingsHelper } from '@features/settings/services/settings-helper';
+import { CoreSettingsHelper, CoreSettingsHelperDevExtraPageItem } from '@features/settings/services/settings-helper';
 import { CoreUserTours } from '@features/usertours/services/user-tours';
 import { CoreCacheManager } from '@services/cache-manager';
 import { CoreConfig } from '@services/config';
@@ -75,6 +75,7 @@ export default class CoreSettingsDevPage implements OnInit {
     autoLoginTimeBetweenRequests?: number;
     lastAutoLoginTime?: number;
 
+    readonly extraPageItems = signal<CoreSettingsHelperDevExtraPageItem[]>([]);
     readonly wsOverrides = signal<{ method: string; count: number }[]>([]);
     readonly totalOverrides = computed(() => this.wsOverrides().reduce((sum, override) => sum + override.count, 0));
 
@@ -89,6 +90,7 @@ export default class CoreSettingsDevPage implements OnInit {
         this.siteId = currentSite?.getId();
 
         this.stagingSitesCount = CoreConstants.CONFIG.sites.filter((site) => site.staging).length;
+        this.extraPageItems.set(CoreSettingsHelper.getDevExtraPageItems());
 
         if (this.stagingSitesCount) {
             this.enableStagingSites = await CoreSettingsHelper.hasEnabledStagingSites();
@@ -281,6 +283,15 @@ export default class CoreSettingsDevPage implements OnInit {
             this.enableStagingSites = !enabled;
             CoreAlerts.showError(error);
         }
+    }
+
+    /**
+     * Open an extra item's page.
+     *
+     * @param item Item to open.
+     */
+    openExtraItem(item: CoreSettingsHelperDevExtraPageItem): void {
+        CoreNavigator.navigate(item.page, { params: item.pageParams });
     }
 
 }
