ff19903a0ea8f16f5133551a2daab97a51158e99
Pau Ferrer Ocaña <crazyserver@gmail.com>
2026-04-29 11:43:14 +0200

    MOBILE-4974 gha: Create base branch fixes

--- .github/scripts/create_base_branch.sh
+++ .github/scripts/create_base_branch.sh
@@ -12,6 +12,7 @@ fi
 print_title "Run create Base Branch scripts"
 git clone --depth 1 --single-branch --branch ionic5 "https://$GIT_TOKEN@github.com/moodlemobile/apps-scripts.git" ../scripts
 cp ../scripts/*.sh scripts/
+cp -R ../scripts/patches scripts/
 
 if [ ! -f scripts/create-base-branch.sh ]; then
     print_error "Create Base Branch file not found"
