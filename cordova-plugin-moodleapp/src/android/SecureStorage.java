0fe64e87cf761c57654ac2ba4bdd1c3cb47abfac
Dani Palou <dani@moodle.com>
2026-09-09 12:31:22 +0200

    MOBILE-5168 android: Fix Android compilation after Push removal
    Our SecureStorage depends on the Push plugin, so the compilation fails. For now the tokens will be stored unencrypted, and it will be encrypted again once the Push plugin is fixed.

--- cordova-plugin-moodleapp/src/android/SecureStorage.java
+++ cordova-plugin-moodleapp/src/android/SecureStorage.java
@@ -28,7 +28,7 @@ import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.PluginResult;
 
-import com.adobe.phonegap.push.EncryptionHandler;
+// import com.adobe.phonegap.push.EncryptionHandler;
 
 public class SecureStorage extends CordovaPlugin {
 
@@ -94,7 +94,9 @@ public class SecureStorage extends CordovaPlugin {
                 continue;
             }
 
-            result.put(name, EncryptionHandler.Companion.decrypt(context, rawValue));
+            // @todo Capacitor
+            // result.put(name, EncryptionHandler.Companion.decrypt(context, rawValue));
+            result.put(name, rawValue);
         }
 
         return result;
@@ -118,7 +120,9 @@ public class SecureStorage extends CordovaPlugin {
             String name = names.optString(i);
 
             if (name != null && !name.isEmpty()) {
-                editor.putString(name, EncryptionHandler.Companion.encrypt(context, data.getString(name)));
+                // @todo Capacitor
+                // editor.putString(name, EncryptionHandler.Companion.encrypt(context, data.getString(name)));
+                editor.putString(name, data.getString(name));
             }
         }
 
